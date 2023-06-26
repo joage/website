f4fd530a6c609eac5af0ee1d76cb7e313cb331a0
George Wang <georgeyw@gmail.com>
2023-06-26 15:48:27 -0400

    Update page.tsx

--- src/app/adventures/test/page.tsx
+++ src/app/adventures/test/page.tsx
@@ -6,17 +6,6 @@ const Test = () => (
     <ImageGroup images={[{path: "/images/IMG_3810.jpeg", caption: "Jagged peaks above Refugio Frey"}]} />
 
     This is a trst commit from george iphone's
-    
-
-    <ImageGroup images={[{path: "/images/valley.jpg", caption: "Climbing above treeline on the second pass of the day"}]} />
-
-    <ImageGroup images={[{path: "/images/descending.jpg", caption: "On the descent to Laguna Jakob"}, {path: "/images/boulders.jpg", caption: "Slow going down a soul-crushing boulder field"}]} />
-
-    <ImageGroup images={[{path: "/images/jakob.jpg", caption: "Laguna Jakob"}]} />
-
-    <ImageGroup images={[{path: "/images/descent.jpg", caption: "Lush valley on the hike out"}]} />
-
-    <ImageGroup images={[{path: "/images/waterfall.jpg", caption: ""}]} />
 
   </Post>
 );
