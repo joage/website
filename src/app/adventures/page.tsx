240ba164767c5c862d88370419777eb71043c9a5
George Wang <wgeorge@berkeley.edu>
2023-06-29 01:58:13 +0200

    Update page.tsx

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -7,7 +7,7 @@ export default function Adventures() {
     <main className="flex flex-col min-h-screen items-center p-4">
       <Nav />
       <div className="flex flex-col gap-6 max-w-3xl pt-8 text-sm">
-        <p>Trip Reports from my hikes and travels. Newest first!</p>
+        <p>Trip reports from my hikes and travels. Newest first!</p>
         <ul className="list-disc text-cyan-600 mx-4">
           <li>
             <Link href="/adventures/leaving-ny">Leaving New York</Link>
