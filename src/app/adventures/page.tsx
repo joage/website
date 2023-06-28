31d0bae0e9722a764739af18ab834537424cd07c
George Wang <wgeorge@berkeley.edu>
2023-06-29 01:56:06 +0200

    Update list page.tsx

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -10,7 +10,19 @@ export default function Adventures() {
         <p>Trip Reports from my adventures. More to come!</p>
         <ul className="list-disc text-cyan-600 mx-4">
           <li>
-            <Link href="/adventures/pat">Patagonia 2023</Link>
+            <Link href="/adventures/leaving-ny">Leaving New York</Link>
+          </li>
+          <li>
+            <Link href="/adventures/shasta">Shasta</Link>
+          </li>
+          <li>
+            <Link href="/adventures/pat">Patagonia</Link>
+          </li>
+          <li>
+            <Link href="/adventures/utah-2">Slot Canyons (Utah Part 2)</Link>
+          </li>
+          <li>
+            <Link href="/adventures/utah-1">Patagonia 2023</Link>
           </li>
         </ul>
       </div>
