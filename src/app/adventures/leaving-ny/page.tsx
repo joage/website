7ebe176a831101cbb827eb115bdbd6a04dcc24fa
George Wang <wgeorge@berkeley.edu>
2023-06-29 01:49:52 +0200

    Update page.tsx

--- src/app/adventures/leaving-ny/page.tsx
+++ src/app/adventures/leaving-ny/page.tsx
@@ -2,14 +2,14 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const Leaving = () => (
-  <Post title="Test" date="June 2023" prev="/adventures/shasta">
+  <Post title="Leaving New York" date="June 26, 2023" prev="/adventures/shasta">
     <ImageGroup images={[{path: "/images/IMG_3810.jpeg", caption: "Goodbye, Brooklyn."}]} />
 
-In June I quit my job in California and decided to travel for a bit this summer.
+    <p>In June I quit my job in California and decided to travel for a bit this summer.</p>
 
-During a week of catching up with old friends in NYC, I made this website to document my hikes and the places i’ll be going to in Europe. In a few days I’ll be walking the Tour du Mont Blanc, and later in July I want to do the GR 10 in the south of France.
+    <p>During a week of catching up with old friends in NYC, I made this website to document my hikes and the places i’ll be going to in Europe. In a few days I’ll be walking the Tour du Mont Blanc, and later in July I want to do the GR 10 in the south of France.</p>
 
-So the trip reports that follow will be written solely on my phone’s notes app and uploaded using github mobile  (bear with me) when I reach wifi. Here I go!
+    <p>So the trip reports that follow will be written solely on my phone’s notes app and uploaded using github mobile  (bear with me) when I reach wifi. Here I go!</p>
 
   </Post>
 );
