893b47cdb250d205dfd693e1eef7a5d526a20bf8
George Wang <wgeorge@berkeley.edu>
2023-06-29 07:47:46 +0200

    Update page.tsx

--- src/app/adventures/utah-1/page.tsx
+++ src/app/adventures/utah-1/page.tsx
@@ -11,7 +11,7 @@ const Coyote = () => (
 
     <p>Needless to say, these trips hardly ever go to plan and we got quickly sidetracked in one Nevada town along the way.</p>
 
-    <ImageGroup images={[{path: "/images/PXL_20221122_231312330_Original.jpeg", caption: ""}]} />
+    <ImageGroup images={[{path: "/images/vegas.jpeg", caption: "Booo, this isnt hiking!"}]} />
     
     Luckily Fischer and Chloe found us a friend’s floor to crash on after our night out so we wouldn’t have to camp. The next morning, craving some more natural scenery, we drove off to our first stop at Bryce Canyon Natl Park. 
 
@@ -19,9 +19,15 @@ const Coyote = () => (
 
     <p>Continuing on after a quick hike, we arrived at the town of Escalante, UT where everything was closed. We turned off the main road on to a dirt track where we pitched tents and spent our first cold night in the desert.</p>
 
+    <ImageGroup images={[{path: "/images/IMG_7625_Original.jpeg", caption: "The Gulch near Crack in the Wall"}]} />
+
     <p>Coyote gulch is a stretch of the Escalante River with steep red rock walls, not narrow enough to be a slot canyon but still requires wading through knee deep sections of icy cold water. Notable arches, natural bridges, and mars-like landscapes made this an ideal “best of” southern Utah destination, according to Fischer who did all the planning this trip.</p>
 
+    <ImageGroup images={[{path: "/images/IMG_2829.jpeg", caption: "Crack in the Wall"}, {path: "/images/IMG_2820.jpeg", caption: "Packs don't fit in the crack"}]} />
+
     <p>In order to access the gulch, we had to continue driving down this dirt track, park, and hike out to a point known as Crack in the Wall. Here we descended a narrow slit, while Fischer lowered our bags by rope over the rim of the canyon.</p>
+
+    <ImageGroup images={[{path: "/images/IMG_2827.jpeg", caption: "Chloe after surviving the crack"}]} />
 
     Hiking inside the gulch felt more like a game of not getting our socks and shoes wet. The four of us each chose different strategies at stream crossings - barefoot wading, risky long jumps with shoes on, or a combo of both. Fortunately it had not rained in a while so water levels were okay. We soon made it to a nice cave that we’d call home for the night.
 
