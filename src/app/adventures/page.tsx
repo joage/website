8f5ff2467cc31338890a151337d61470f03018fb
George Wang <georgeyw@gmail.com>
2023-06-29 02:11:26 +0200

    Leaving ny (#3)

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -7,10 +7,22 @@ export default function Adventures() {
     <main className="flex flex-col min-h-screen items-center p-4">
       <Nav />
       <div className="flex flex-col gap-6 max-w-3xl pt-8 text-sm">
-        <p>Trip Reports from my adventures. More to come!</p>
+        <p>Trip reports from my hikes and travels. Newest first!</p>
         <ul className="list-disc text-cyan-600 mx-4">
           <li>
-            <Link href="/adventures/pat">Patagonia 2023</Link>
+            <Link href="/adventures/leaving-ny">Leaving New York</Link>
+          </li>
+          <li>
+            <Link href="/adventures/shasta">Shasta</Link>
+          </li>
+          <li>
+            <Link href="/adventures/pat">Patagonia</Link>
+          </li>
+          <li>
+            <Link href="/adventures/utah-2">Slot Canyons (Utah Part 2)</Link>
+          </li>
+          <li>
+            <Link href="/adventures/utah-1">Coyote Gulch (Utah Part 1)</Link>
           </li>
         </ul>
       </div>
