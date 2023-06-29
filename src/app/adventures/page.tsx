29ca9632a28285c3890b6f65485cf46cff99bc8a
George Wang <wgeorge@berkeley.edu>
2023-06-29 02:04:56 +0200

    Update page.tsx

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -22,7 +22,7 @@ export default function Adventures() {
             <Link href="/adventures/utah-2">Slot Canyons (Utah Part 2)</Link>
           </li>
           <li>
-            <Link href="/adventures/utah-1">Patagonia 2023</Link>
+            <Link href="/adventures/utah-1">Coyote Gulch (Utah Part 1)</Link>
           </li>
         </ul>
       </div>
