68234a825b4000fdf4854619ba4601615ee41213
George Wang <georgeyw@gmail.com>
2023-06-30 03:53:07 +0200

    utah coyote gulch (#5)

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -19,13 +19,13 @@ export default function Adventures() {
             <Link href="/adventures/pat">Patagonia</Link>
           </li>
           <li>
-            WIP - Slot Canyons (Utah Part 2)
+            <Link href="/adventures/utah-2">Slot Canyons (Utah Part 2)</Link>
           </li>
           <li>
-            WIP - Coyote Gulch (Utah Part 1)
+            <Link href="/adventures/utah-1">Coyote Gulch (Utah Part 1)</Link>
           </li>
         </ul>
-        <p>Will add older trips when I have time!</p>
+        <p>Will slowly add older trips when I find the time</p>
       </div>
     </main>
   )
