91e1f3b30d42b9cc036d085b105a78cfee3368ce
George Wang <wgeorge@berkeley.edu>
2023-06-30 03:16:46 +0200

    Update page.tsx

--- src/app/adventures/utah-2/page.tsx
+++ src/app/adventures/utah-2/page.tsx
@@ -2,9 +2,14 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const Canyon = () => (
-  <Post title="Spooky abd Peekaboo Canyons (Utah Part 2)" date="November 2022" prev="/adventures/utah-1" next="/adventures/pat">
-    <ImageGroup images={[{path: "/images/PXL_20221122_231312330_Original.jpeg", caption: "Looking up at Jacob Hamblin Arch"}]} />
+  <Post title="Spooky and Peekaboo Canyons (Utah Part 2)" date="November 2022" prev="/adventures/utah-1" next="/adventures/pat">
+    
+    A collection of images from our walk through Spooky and Peekaboo Canyon, the day after completing Coyote Gulch (see previous post)!
+    <ImageGroup images={[{path: "/images/PXL_20221123_185423308.MP_Original.jpeg", caption: ""}, {path: "/images/PXL_20221123_190113622.MP_Original.jpeg", caption: ""}]} />
+    <ImageGroup images={[{path: "/images/IMG_7702_Original.jpeg", caption: ""}, {path: "/images/IMG_7725_Original.jpeg", caption: ""}]} />
+    <ImageGroup images={[{path: "/images/PXL_20221123_202531125.MP_Original.jpeg", caption: ""}, {path: "/images/IMG_7705_Original.jpeg", caption: ""}]} />
 
+    <ImageGroup images={[{path: "/images/PXL_20221123_214147315_Original.jpeg", caption: ""}]} />
   </Post>
 );
 
