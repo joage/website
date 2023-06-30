bcbc070dfffa0e888598688b6c632c674b75cdcb
George Wang <wgeorge@berkeley.edu>
2023-06-30 03:46:57 +0200

    fix typos

--- src/app/adventures/utah-2/page.tsx
+++ src/app/adventures/utah-2/page.tsx
@@ -8,15 +8,14 @@ const Canyon = () => (
     <ImageGroup images={[{path: "/images/PXL_20221123_185423308.MP_Original.jpeg", caption: ""}, {path: "/images/PXL_20221123_190113622.MP_Original.jpeg", caption: ""}]} />
     <ImageGroup images={[{path: "/images/IMG_7702_Original.jpeg", caption: ""}, {path: "/images/IMG_7725_Original.jpeg", caption: ""}]} />
     <ImageGroup images={[{path: "/images/IMG_7705_Original.jpeg", caption: ""}, {path: "/images/PXL_20221123_202531125.MP_Original.jpeg", caption: ""}]} />
-    <ImageGroup images={[{path: "/images/PXL_20221123_204050247_Original.jpeg", caption: ""}, {path: "public/images/PXL_20221123_204517817_Original.jpeg", caption: ""}]} />
-    <ImageGroup images={[{path: "/images/PXL_20221127_175135918_Original.jpeg", caption: ""}]} />
+    <ImageGroup images={[{path: "/images/PXL_20221123_204050247_Original.jpeg", caption: ""}, {path: "/images/PXL_20221123_204517817_Original.jpeg", caption: ""}]} />
 
-    Hot springs and back to CA, the days after thanksgiving
+    After thanksgiving, we hit some hot springs and then drove back to CA.
 
     <ImageGroup images={[{path: "/images/IMG_2882.jpeg", caption: ""}, {path: "images/IMG_2901_Original.jpeg", caption: ""}]} />
     <ImageGroup images={[{path: "/images/PXL_20221127_175135918_Original.jpeg", caption: ""}]} />
 
-    Most of the pictures featured here aren't mine and are actually taken by Fischer, Emma, and Chloe! The Heimburgers were awesome hosts, thanks for an amazing thanksgiving dinner!"
+    Most of the pictures featured here aren't mine and are actually taken by Fischer, Emma, and Chloe! The Heimburgers were awesome hosts, thanks for an amazing thanksgiving dinner!
   </Post>
 );
 
