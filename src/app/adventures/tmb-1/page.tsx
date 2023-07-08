6b0e65ac6fa492ea484a96e22b1bae0aa06fe054
George Wang <georgeyw@gmail.com>
2023-07-08 13:28:37 +0200

    tmb day 1 text

--- src/app/adventures/tmb-1/page.tsx
+++ src/app/adventures/tmb-1/page.tsx
@@ -2,10 +2,21 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const TMB = () => (
-  <Post title="Courmayeur (TMB Part 1)" date="July 1, 2023" prev="/adventures/leaving-ny">
-    <ImageGroup images={[{path: "/images/IMG_3911.jpeg", caption: "Bike ride in Geneva"}]} />
+  <Post title="Courmayeur (TMB Part 1)" date="July 2, 2023" prev="/adventures/leaving-ny">
+    <ImageGroup images={[{path: "/images/IMG_3993.jpeg", caption: ""}]} />
 
-    <p>In June I quit my job in California and decided to travel for a bit this summer.</p>
+    <p>On July 1st Emma and I met up in Geneva. From here we boarded a bus to Courmayeur, Italy to begin our Tour du Mont Blanc half circuit. Over the next 5 days we would traverse alpine terrain across 3 countries, staying in huts and hostels along the way, before arriving in Chamonix, France. Manifesting good weather, food, and company, we both had high hopes for an epic European adventure.</p>
+
+Day 1 began with a 1300 meter climb. We left Courmayeur around 8am, fueled by pizza from the night before and excitement to be in the mountains.
+
+After passing Rifugio Bertone, we decided to take the alternate route up Mont de la Saxe, since we were making good time so far. This route follows a ridge line up to 2500m before dipping down to Col Sapin. We met an older French couple who was following the same path as us; we’d end up having dinner with them later.
+
+The alternate route joined back up with the TMB at Rifugio Bonatti, a super scenic hut against the backdrop of the Mont Blanc Massif. We continued on, traversing some melting snow fields and passing by old ruins of former alpine lodgings. 
+
+At the end of the final ascent, I thought we still had more to go but Emma informed me we had arrived at our destination, Rifugio Elena. Great timing because I was exhausted at this point and we made it just in time for dinner at the refuge!
+
+Day 1 ended with 2000m of elevation gain and 23km traveled. The scenery today set the bar high for the next four days to come!
+
 
   </Post>
 );
