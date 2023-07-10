e9d48f779fe1b3134c0f82ee146c2dc6f83251b7
George Wang <georgeyw@gmail.com>
2023-07-10 13:04:32 +0200

    text copy

---
+++ src/app/adventures/tmb-2/page.tsx
@@ -0,0 +1,28 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
+
+const TMB = () => (
+  <Post title="Entering Switzerland (TMB Day 2)" date="July 3, 2023" prev="/adventures/tmb-1">
+    <ImageGroup images={[{path: "/images/tmb/IMG_4066.jpeg", caption: "Switzerland!"}]} />
+
+    It was a spectacularly clear morning at Rifugio Elena. After breakfast we began the steep ascent up to the Grand Col Ferret and into Switzerland, watching the valley below get smaller and smaller with each step we climbed.
+    
+    <ImageGroup images={[{path: "/images/tmb/IMG_4048.jpeg", caption: "Watching clouds roll over Col Ferret. "}]} />
+
+    At the top of Col Ferret, we said goodbye to Italy and descended into the land of chard, cheese, and chocolates.
+    
+    <ImageGroup images={[{path: "/images/tmb/IMG_4058.jpeg", caption: "Walking into the clouds"}]} />
+
+As soon as we descended down below the clouds it was as if we entered a different world, with entirely new scenery. Switzerland’s valleys were lush, green, and filled with wildflowers (and pollen, unfortunately for my allergies). 
+
+A quick stop in the town of La Fouly to resupply on food later, we continued on towards Champex-Lac. This section of trail featured bucolic Swiss towns instead of grand mountain views; we could not see the Mont Blanc Massif much.
+
+Finally it was time to ascend again, back into the mountains. Signs and plaques motivated us on the way up to Champex-Lac, giving us helpful insight on the native mushroom species of the area.
+
+In Champex we had a curious dinner composed of fish and chips and traditional Swiss rosti, before sharing our hostel with a large guided tour group, one of many which we had seen on the trail. 
+
+Day 2 ended with 26.05km traveled and 996m of elevation gain.
+  </Post>
+);
+
+export default TMB;
