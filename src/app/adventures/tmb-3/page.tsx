b49d794e1c1566f73ad0afd5315f55733a7353e0
George Wang <georgeyw@gmail.com>
2023-07-11 10:32:30 +0200

    Update page.tsx

--- src/app/adventures/tmb-3/page.tsx
+++ src/app/adventures/tmb-3/page.tsx
@@ -1,1 +1,22 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
 
+const TMB = () => (
+  <Post title="Champex to Finhaut (TMB Day 3)" date="July 4, 2023" prev="/adventures/tmb-2">
+    <ImageGroup images={[{path: "/images/tmb/IMG_4066.jpeg", caption: "Cows of La Peule"}]} />
+
+Since yesterday was so long, today the goal was to chill and take the flatter of two routes to Trient. We left Champex in no rush, thinking it would be an easy day with not so much climbing. That plan quickly devolved when we came across the fork, and saw a huge guided group head the way we were planning to go. Emma and I discussed our options a bit and decided instead to go for the high route through the Fenêtre d’Arpette.
+
+The moment we entered the Val (valley) d’Arpette we knew that we had made the right move. Rising slowly at first through green meadows and pine forest, then sharply over rocky, snow-streaked terrain, this trail reminded me of the approaches to high peaks back home in California’s eastern sierra.
+
+At —— ft, after —— ft of ascent, we crested the col and peered into the Trient valley below.
+
+We took in the views, had lunch, and started the long descent to Trient with no shade. 
+
+At the Buvette du Glacier, dunking our heads in the river provided nice relief from the harsh afternoon sun.
+
+At around 5, we strolled into Trient but our day didn’t end there. Because of how late we had planned this trip we had to book a stay in the next town over in Le Châtelard - Finhaut. Feeling hungry and not wanting to wait for the bus to come at 7:30, we tried to hitch a ride by the road and were instantly picked up by a kind local. After he dropped us off in Le Châtelard we climbed the final 200m to our gîte for the night, hosted by a really nice lady, Jacqueline, who was retired and had 3 kittens. Dinner was family style and incredibly good—it was so worth the extra mileage to come here!
+  </Post>
+);
+
+export default TMB;
