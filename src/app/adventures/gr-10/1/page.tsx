75e99f5890c67a48455df5942fa94cfdd88adb17
George Wang <wgeorge@berkeley.edu>
2023-07-16 01:37:41 +0200

    Update page.tsx

--- src/app/adventures/gr-10/1/page.tsx
+++ src/app/adventures/gr-10/1/page.tsx
@@ -3,31 +3,40 @@ import Post from '@/components/Post';
 
 const GR = () => (
   <Post title="Rain! (GR 10 Day 1)" date="July 11, 2023" prev="/adventures/tmb-6">
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Atlantic Ocean! Starting my 50 day trek to the Mediterranean Sea."}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Atlantic Ocean! Starting my 50 day journey to the Mediterranean Sea."}]} />
 
-On July 10th I boarded my sleeper train at Gare d’Austerlitz in Paris. The TGV would have been a faster ride, but this train somehow only cost 19 Euro.
+    On July 10th I boarded a sleeper train at Gare d’Austerlitz in Paris. The TGV would have been a faster ride, but this train somehow only cost 19 Euro.
 
-After 14 hrs I arrived in Hendaye, a beach resort town by Spain where I would begin my attempt to thru-hike the GR 10 across the Pyrenees. As I dipped my feet in the sea, I was super excited to get started!
+    After 14 hrs I arrived in Hendaye, a beach resort town on tge Atlantic where I would start my attempt to thru-hike the GR 10 across the Pyrenees. As I dipped my feet in the sea, I was super excited to begin!
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4476.jpeg", caption: "Hendaye"}, {path: "/images/gr-10/IMG_4489.jpeg", caption: "Let's go!"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4476.jpeg", caption: "Hendaye, France"}, {path: "/images/gr-10/IMG_4489.jpeg", caption: "Let's go!"}]} />
 
 At around noon, I left Hendaye. Paved streets turned into dirt paths, then gentle forest trails. Moist is how I’d best describe the feeling during this part of the day but that was soon to change.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4485.jpeg", caption: ""}]} />
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4486.jpeg", caption: "Scenes from the Basque countryside"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4486.jpeg", caption: ""}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4491.jpeg", caption: "Scenes from the Basque countryside"}]} />
 
 Passing the tiny town of Biriatou, I continued up the sides of larger and larger hills. Gradually, the humidity turned into real rain the higher I climbed. I went up and down a big ridge, but couldn’t see anything on either side.
 
-Suddenly a restaurant appeared out of the mist. I was in Spain, just along the border but they spoke French as well. I ordered a drink here and stayed a bit too long trying to get dry, only to go back out into the rain. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4491.jpeg", caption: ""}]} />
 
-Past the Col d’Ibardin is where I made my mistake. My maps app showed a shortcut that would get me to Ohlette faster, so I decided to take it. This “trail” started out nice but turned into a longcut because of the intense bushwhack and steep decline, all in the worsening rain. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4503.jpeg", caption: ""}]} />
 
-Puddles were forming everywhere, and pretty soon I was soaked through
+    Suddenly a restaurant appeared out of the mist. I was in Spain, just along the border but they spoke French as well. I ordered a drink here and stayed a bit too long trying to get dry, only to go back out into the rain. 
 
-When I made it to Ohlette, it was 8pm and I tried the first gîte I saw to see if I could pitch my tent there, or even sleep inside. Luckily for me the host was a nice man and he showed me his single-occupancy treehouse in the yard next to the chickens, and let me sleep in it for 10 euro. I cooked some dinner, hung up my dripping clothes, and went to bed.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4501.jpeg", caption: ""}]} />
 
+    <p>Past the Col d’Ibardin is where I made my mistake. My maps app showed a shortcut that would get me to Ohlette faster, so I decided to take it. This “trail” started out nice but turned into a longcut because of the intense bushwhack and steep decline, all in the worsening rain. Bushwhacking pretty much guaranteed that I was going to be soaked through.</p>
 
-Day 1 was quite a start. I spent way too much time in Hendaye and at the cafe, so I got wet as a result. A lesson learned here is to not take alternate trails without checking the steepness!
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4507.jpeg", caption: ""}]} />
+
+    When I made it to Ohlette, it was 8pm and I tried the first gîte I saw to see if I could pitch my tent there, or even sleep inside. Luckily for me the host was a nice man and he showed me his single-occupancy treehouse in the yard next to the chickens, and let me sleep in it for 10 euro. I cooked some dinner, hung up my dripping clothes, and went to bed.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4508.jpeg", caption: "Slept in the treehouse next to the chicken coop. Twas cozy"}]} />
+
+    Day 1 was quite a start. I spent way too much time in Hendaye and at the cafe, so I got wet as a result. A lesson learned here is to not take alternate trails without checking the steepness!
 
 
   </Post>
