fbe44f41ef8651cc41dbd79911ef8e4207cf5a99
George Wang <wgeorge@berkeley.edu>
2023-07-16 01:46:30 +0200

    Update page.tsx

--- src/app/adventures/gr-10/1/page.tsx
+++ src/app/adventures/gr-10/1/page.tsx
@@ -7,7 +7,7 @@ const GR = () => (
 
     On July 10th I boarded a sleeper train at Gare d’Austerlitz in Paris. The TGV would have been a faster ride, but this train somehow only cost 19 Euro.
 
-    After 14 hrs I arrived in Hendaye, a beach resort town on tge Atlantic where I would start my attempt to thru-hike the GR 10 across the Pyrenees. As I dipped my feet in the sea, I was super excited to begin!
+    After 14 hrs I arrived in Hendaye, a beach resort town on the Atlantic where I would start my attempt to thru-hike the GR 10 across the Pyrenees. As I dipped my feet in the sea, I was super excited to begin!
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4476.jpeg", caption: "Hendaye, France"}, {path: "/images/gr-10/IMG_4489.jpeg", caption: "Let's go!"}]} />
 
@@ -20,13 +20,12 @@ At around noon, I left Hendaye. Paved streets turned into dirt paths, then gentl
 
 Passing the tiny town of Biriatou, I continued up the sides of larger and larger hills. Gradually, the humidity turned into real rain the higher I climbed. I went up and down a big ridge, but couldn’t see anything on either side.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4491.jpeg", caption: ""}]} />
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4503.jpeg", caption: ""}]} />
 
-    Suddenly a restaurant appeared out of the mist. I was in Spain, just along the border but they spoke French as well. I ordered a drink here and stayed a bit too long trying to get dry, only to go back out into the rain. 
-
     <ImageGroup images={[{path: "/images/gr-10/IMG_4501.jpeg", caption: ""}]} />
+
+    Suddenly a restaurant appeared out of the mist. I was in Spain, just along the border but they spoke French as well. I ordered a drink here and stayed a bit too long trying to get dry, only to go back out into the rain. 
 
     <p>Past the Col d’Ibardin is where I made my mistake. My maps app showed a shortcut that would get me to Ohlette faster, so I decided to take it. This “trail” started out nice but turned into a longcut because of the intense bushwhack and steep decline, all in the worsening rain. Bushwhacking pretty much guaranteed that I was going to be soaked through.</p>
 
@@ -36,9 +35,9 @@ Passing the tiny town of Biriatou, I continued up the sides of larger and larger
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4508.jpeg", caption: "Slept in the treehouse next to the chicken coop. Twas cozy"}]} />
 
-    Day 1 was quite a start. I spent way too much time in Hendaye and at the cafe, so I got wet as a result. A lesson learned here is to not take alternate trails without checking the steepness!
+    Day 1 was quite the start. I spent way too much time in Hendaye and at the cafe, so I got wet as a result. Hope day 2 gives me some better weather!
 
-
+<p>Hendaye to Ohlette: 28.84km and 975m of elev. gain</p>
   </Post>
 );
 
