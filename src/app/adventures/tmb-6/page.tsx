788627e4b3dfd9072f2d3028296240b6c393e143
George Wang <wgeorge@berkeley.edu>
2023-07-15 23:34:50 +0200

    Update page.tsx

--- src/app/adventures/tmb-6/page.tsx
+++ src/app/adventures/tmb-6/page.tsx
@@ -1,1 +1,28 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
+
+const TMB = () => (
+  <Post title="Wine Tour du Mont Blanc" date="July 2023" prev="/adventures/tmb-5" next="/adventures/gr-10/1">
+    <ImageGroup images={[{path: "/images/tmb/IMG_4798_Original.jpeg", caption: ""}]} />
+
+    <p>Since yesterday was so long, today the goal was to chill and take the flatter of two routes to Trient. We left Champex in no rush, thinking it would be an easy day with not so much climbing. That plan quickly devolved when we came across the fork, and saw a huge guided group head the way we were planning to go. Emma and I discussed our options a bit and decided instead to go for the high route through the Fenêtre d’Arpette.</p>
+
+    Today was one long climb. Emma and I were both excited for the refuge we booked for the night on top of a glacier, but first we had to do some route-finding to get out of Finhaut.
+
+Since todays route was not on the official Tour du Mont Blanc, we had to find our own way through the extensive network of trails up to the Col de Balme. Even though it was steep, I enjoyed the relative emptiness of this section compared to the highway that was the TMB.
+
+Upon reaching Col de Balme, I had the most amazing omelette in the refuge—we were now in France!
+
+After lunch we had some more climbing to do, luckily the thunderstorm warnings went away. The last section of trail crossed boulder fields and patches of snow to reach Refuge Albert Premier.
+
+This was the first time we reached our destination relatively early, so we had plenty of time we didn’t know what to do with. Thankfully there were other people at the hut in the same boat so we talked to a hiker doing the Haute Route, some Belgian mountaineers, and a couple on their honeymoon.
+
+those are not paintings
+
+Super cool hut, really fun hike today. Day 4 ended with 13km and 1668m of elevation gained.
+  </Post>
+);
+
+export default TMB;
+
 
