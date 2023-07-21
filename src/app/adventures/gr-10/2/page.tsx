4d3b6ac3f73493f5ce5943c5c00a2890bd4f6092
George Wang <wgeorge@berkeley.edu>
2023-07-21 18:48:22 +0200

    Update page.tsx

--- src/app/adventures/gr-10/2/page.tsx
+++ src/app/adventures/gr-10/2/page.tsx
@@ -2,42 +2,35 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Rain! (GR 10 Day 1)" date="July 11, 2023" prev="/adventures/tmb-6">
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Atlantic Ocean! Starting my 50 day journey to the Mediterranean Sea."}]} />
+  <Post title=" (GR 10 Day 2)" date="July 12, 2023" prev="/adventures/gr-10/1">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4521.jpeg", caption: "Day 2 comes with views"}]} />
 
-    On July 10th I boarded a sleeper train at Gare d’Austerlitz in Paris. The TGV would have been a faster ride, but this train somehow only cost 19 Euro.
+    The day started with a climb to a small Col, where I suddenly heard voices yelling. When I got closer it turned out there was a mountain railroad carrying people up to La Rhûne, a 900m peak. Since it was cloudy and my left foot still felt sore from the TMB, I elected to not climb La Rhûne and just make it to Sare as quickly as possible.
 
-    After 14 hrs I arrived in Hendaye, a beach resort town on the Atlantic where I would start my attempt to thru-hike the GR 10 across the Pyrenees. As I dipped my feet in the sea, I was super excited to begin!
+<ImageGroup images={[{path: "/images/gr-10/IMG_4526.jpeg", caption: "Mountain railroad"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4476.jpeg", caption: "Hendaye, France"}, {path: "/images/gr-10/IMG_4489.jpeg", caption: "Let's go!"}]} />
+At Sare I had a big meal to make up for my hasty dinner last night. I thought Sare was a touristy town but nothing would prepare me for the touristyness in Ainhoa, where I was headed.
 
-At around noon, I left Hendaye. Paved streets turned into dirt paths, then gentle forest trails. Moist is how I’d best describe the feeling during this part of the day but that was soon to change.
+<ImageGroup images={[{path: "/images/gr-10/IMG_4530.jpeg", caption: "Trying a Gâteau Basque, a custard filled pastry"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4485.jpeg", caption: ""}]} />
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4486.jpeg", caption: ""}]} />
+The path to Ainhoa followed rural roads where little kids rode bikes, then dipped into ravines where streams separated farmers fields.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4491.jpeg", caption: "Scenes from the Basque countryside"}]} />
+<ImageGroup images={[{path: "/images/gr-10/IMG_4537.jpeg", caption: "Walking towards Ainhoa, La Rhûne now visible behind me"}]} />
 
-Passing the tiny town of Biriatou, I continued up the sides of larger and larger hills. Gradually, the humidity turned into real rain the higher I climbed. I went up and down a big ridge, but couldn’t see anything on either side.
+<ImageGroup images={[{path: "/images/gr-10/IMG_4539.jpeg", caption: "Super straight road formed the border between France and Spain"}]} />
 
+  Walking into Ainhoa on the main road felt a bit like disneyland. The houses were painted uniformly and nobody looked local. The lady at the tourism office told me there are no grocery shops or even stores, other than Basque gift shops, in town. I got a quick bite to eat at a restaurant, prepared myself to potentially stealth camp tonight, then left once again for the mountains.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4503.jpeg", caption: ""}]} />
+<ImageGroup images={[{path: "/images/gr-10/IMG_4546.jpeg", caption: "Ainhoa"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4501.jpeg", caption: ""}]} />
+The final climb was super hot, and no trees offered me shade. When i made it to the church where I planned to pitch my tent I was relieved to see 4-5 other tents already set up, so apparently camping was allowed here. There was even a trash bin and a drinking water fountain.
 
-    Suddenly a restaurant appeared out of the mist. I was in Spain, just along the border but they spoke French as well. I ordered a drink here and stayed a bit too long trying to get dry, only to go back out into the rain. 
+<ImageGroup images={[{path: "/images/gr-10/IMG_4553.jpeg", caption: "Chappelle Notre Dame de l’Aubépine, my campsite"}]} />
 
-    <p>Past the Col d’Ibardin is where I made my mistake. My maps app showed a shortcut that would get me to Ohlette faster, so I decided to take it. This “trail” started out nice but turned into a longcut because of the intense bushwhack and steep decline, all in the worsening rain. Bushwhacking pretty much guaranteed that I was going to be soaked through.</p>
+The view of Ainhoa and La Rhûne was great here, and I could finally see come far I walked today. Sheep’s bells lulled me to sleep.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4507.jpeg", caption: ""}]} />
-
-    When I made it to Ohlette, it was 8pm and I tried the first gîte I saw to see if I could pitch my tent there, or even sleep inside. Luckily for me the host was a nice man and he showed me his single-occupancy treehouse in the yard next to the chickens, and let me sleep in it for 10 euro. I cooked some dinner, hung up my dripping clothes, and went to bed.
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4508.jpeg", caption: "Slept in the treehouse next to the chicken coop. Twas cozy"}]} />
-
-    Day 1 was quite the start. I spent way too much time in Hendaye and at the cafe, so I got wet as a result. Hope day 2 brings some better weather!
-
-<p>Hendaye to Ohlette: 28.84km and 975m of elevation gained.</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4558.jpeg", caption: "First sunset of the trip!"}]} />
+    <p>
   </Post>
 );
 
