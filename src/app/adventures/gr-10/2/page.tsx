9ffc7f65951b55171249dc5986f0196d5303e350
George Wang <wgeorge@berkeley.edu>
2023-07-21 18:49:55 +0200

    Update page.tsx

--- src/app/adventures/gr-10/2/page.tsx
+++ src/app/adventures/gr-10/2/page.tsx
@@ -2,7 +2,7 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title=" (GR 10 Day 2)" date="July 12, 2023" prev="/adventures/gr-10/1">
+  <Post title="Ainhoa (GR 10 Day 2)" date="July 12, 2023" prev="/adventures/gr-10/1">
     <ImageGroup images={[{path: "/images/gr-10/IMG_4521.jpeg", caption: "Day 2 comes with views"}]} />
 
     The day started with a climb to a small Col, where I suddenly heard voices yelling. When I got closer it turned out there was a mountain railroad carrying people up to La Rhûne, a 900m peak. Since it was cloudy and my left foot still felt sore from the TMB, I elected to not climb La Rhûne and just make it to Sare as quickly as possible.
@@ -30,7 +30,6 @@ The final climb was super hot, and no trees offered me shade. When i made it to 
 The view of Ainhoa and La Rhûne was great here, and I could finally see come far I walked today. Sheep’s bells lulled me to sleep.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4558.jpeg", caption: "First sunset of the trip!"}]} />
-    <p>
   </Post>
 );
 
