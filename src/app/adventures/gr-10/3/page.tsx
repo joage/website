814f45ed1134a431fb9eef7dbfde88af046190f4
George Wang <wgeorge@berkeley.edu>
2023-08-01 20:56:26 +0200

    Update page.tsx test

--- src/app/adventures/gr-10/3/page.tsx
+++ src/app/adventures/gr-10/3/page.tsx
@@ -3,33 +3,7 @@ import Post from '@/components/Post';
 
 const GR = () => (
   <Post title="Bidarray (GR 10 Day 3)" date="July 13, 2023" prev="/adventures/gr-10/2" next="/adventures/gr-10/4">
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4575.jpeg", caption: "Farm life"}]} />
-
-    Day 3 began with an easy traverse to Col des Veaux where I planned to eat lunch at a restaurant in Spain right across the border. Low hanging clouds obscured the views during my walk.
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4564.jpeg", caption: "Foresty morning walk"}]} />
-
-    Arriving at the restaurant Venta Burkaitz, I found out they didnt open until noon and I was an hour early. Not keen on waiting, I continued on and ate my own lunch, my last can of Danish fish with some bread, at the next Col.
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4570".jpeg", caption: "Lunch at Mehatseko Lepoa"}]} />
-
-    The descent to Bidarray was simple because I met a father-son duo from Angers who told me to take the easy way instead of the steep way the guidebook recommends. My legs thanked them for the tip. When we reached town the same duo nicely offered me a beer at the bar-restaurant, which I enjoyed while I waited for the grocery store to reopen after their siesta.
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4572".jpeg", caption: "Gentle track leading to Bidarray"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4578".jpeg", caption: "Approaching town"}]} />
-
-    Once resupplied, I began the final climb of the day, a big one. Some guy I met said they were going to camp at pic d’Iparla around 1000m. I thought that was a crazy but fun idea, so I tried to see how far up I could get before it got too windy.
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4582".jpeg", caption: "A hot and sweaty type of climb"}]} />
-
-    I made it to 800m elevation before selecting a spot to pitch my tent. Other than the abundant sheep droppings, this was a great campsite—on a ridge, with a fabulous sunset, better than yesterday’s. So far, the GR 10 does not disappoint, especially with camping!
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4602".jpeg", caption: "Camping on a ridge!"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4607".jpeg", caption: "Day 3 in the books"}]} />
-
-   A bit after Ainhoa to Crête d'Iparla - 23.8km and 1347m gained.
+    
   </Post>
 );
 
