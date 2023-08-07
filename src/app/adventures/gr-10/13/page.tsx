ce90d07391b4589738b8c4beee16fa540de2969e
George Wang <wgeorge@berkeley.edu>
2023-08-07 14:43:57 +0200

    Update page.tsx 13

--- src/app/adventures/gr-10/13/page.tsx
+++ src/app/adventures/gr-10/13/page.tsx
@@ -9,21 +9,29 @@ const GR = () => (
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4941.jpeg", caption: "Beautiful morning"}]} />
 
-As I descended from Arlet, high mountain ridges turned into shaded forest paths.
+As I descended from Arlet, high mountain ridges turned into shaded forest paths. At a river crossing I chose one of two available routes which took me up to a lake where families were having lunch.
 
-At a river crossing I chose one of two available routes which took me up to a lake where families were having lunch. I ate my sandwich here and joined up with the GR 11, a long distance path through the Spanish side of the Pyrenees.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4947.jpeg", caption: "Ibon de Estanés"}]} />
 
-The GR 11 felt different from the GR 10. It was less traveled and involved some route finding.
+I ate my sandwich here and joined up with the GR 11, a long distance path through the Spanish side of the Pyrenees.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4948.jpeg", caption: "On the GR 11"}]} />
+
+The GR 11 felt different from the GR 10. The trail was rougher and less traveled.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4952.jpeg", caption: "Dipping into a forest"}]} />
 
 When I made it to Candanchu, a Spanish ski town, it was 3pm. I had a drink with a German hiker while waiting for the food shop to reopen after their siesta. The town was pretty empty but at least there was lots of jamón in the grocery store!
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4954.jpeg", caption: "Ski resort at Candanchu"}]} />
+
 After resupplying, I realized I was not going to make it back into France today. The final part of the day involved some road walking and then a big climb to a lake.
 
-Since I was low on cash, I tried to find an ATM in Astun. In my broken Spanish I asked some locals but they were only able to direct me to a wall which used to have an ATM. I hope some places on the trail coming up accept card!
+<p>Since I was low on cash, I tried to find an ATM in Astún. In my broken Spanish I asked some locals for help but they were only able to direct me to a wall where there used to be an ATM. Mission failed.</p>
 
 The final climb was windy, and when I arrived at the lake the wind picked up speed. I knew it was a bad idea to pitch my tent here, but I didn’t want to keep hiking. I cooked my dinner and hoped the wind would die down so I could get some sleep.
 
-Refuge Jeandel to Pic d'Anie to Cabane d'Ardinet - 15.00km and 1280m gained.
+<p>Refuge d'Arlet to Ibon de Escalar - 27.34km and 1381m gained.</p>
   </Post>
 );
 
