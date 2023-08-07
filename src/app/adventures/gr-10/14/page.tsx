a8729a1ec12c14eee62b084e45cb97a884623579
George Wang <georgeyw@gmail.com>
2023-08-07 13:50:50 +0200

    Create page.tsx 14

---
+++ src/app/adventures/gr-10/14/page.tsx
@@ -0,0 +1,40 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
+
+const GR = () => (
+  <Post title="Pombie (HRP Day 14)" date="July 24, 2023" prev="/adventures/gr-10/13" next="/adventures/gr-10/15">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4812.jpeg", caption: "Pic d'Anie pointing toward the sky"}]} />
+
+    I woke up this morning to a fabulous view of the Pic d’Anie from outside my tent. For once the morning was clear and my tent wasn’t even wet! Tristan and I left at around 8:30 to begin our climb of the 2500m peak.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4809.jpeg", caption: "A change in scenery from previous days"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4810.jpeg", caption: "Tristan approaching the limestone"}]} />
+
+The path followed the HRP over a rocky landscape to the Col des Anies, where I dropped my pack. The conditions seemed perfect, a little hot but breezy, ideal for summiting.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4816.jpeg", caption: "Traversing the base of the peak"}]} />
+
+The final 500m to the top did not feel too difficult, especially without a heavy load on my back. 
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4817.jpeg", caption: "High above the clouds"}]} />
+
+At the top we got a stunning panorama of the High Pyrenees peaks, notably Pic du Midi d’Ossau in the foreground. Even though we were not that high up (2500m), this summit felt super epic because of the low clouds covering the valleys below.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4821.jpeg", caption: "Summit! 2500m"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4832.jpeg", caption: "Top, looking down"}]} />
+
+On the way down, I said goodbye to Tristan for now. He headed back to Refuge Jeandel for one more night, and I made my way down the other side of the Col des Anies toward Lescun.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4842.jpeg", caption: "The valley leading to Lescun"}]} />
+
+After I descended 1000m, I passed the first cabane which was occupied by a shepherd. I continued on and planted my tent across the trail from the closed Cabane d’Ardinet.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4845.jpeg", caption: "Met two of these at Cabane d'Ardinet!"}]} />
+
+Refuge Jeandel to Pic d'Anie to Cabane d'Ardinet - 15.00km and 1280m gained.
+  </Post>
+);
+
+export default GR;
