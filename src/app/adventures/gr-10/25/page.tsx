33976f213616846a728b1db48f46b007fcba0b8d
George Wang <wgeorge@berkeley.edu>
2023-08-08 22:10:51 +0200

    Update page.tsx 25

--- src/app/adventures/gr-10/25/page.tsx
+++ src/app/adventures/gr-10/25/page.tsx
@@ -5,13 +5,29 @@ const GR = () => (
   <Post title="Snowstorm (HRP Day 25)" date="August 4, 2023" prev="/adventures/gr-10/24" next="/adventures/gr-10/26">
     <ImageGroup images={[{path: "/images/gr-10/IMG_5394.jpeg", caption: "A wintry landscape"}]} />
 
+    I woke up late again, thinking it would be a short day. It was only 11km to Portillon. When I set off into the mist, I was dressed in shorts and a t-shirt, with only a thin rain jacket. Crucially, my warmest clothes lay stuffed at the bottom of my backpack...
+
     <ImageGroup images={[{path: "/images/gr-10/IMG_5374.jpeg", caption: "Eerie fog in the morning"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5376.jpeg", caption: "When I reached this lake, temperatures dropped and there was heavy rain"}]} />
+The climb started gradually, switchbacking up the valley to a lake with a dam. The mist turned into rain, but I did not feel concerned yet. This type of rain was familiar to me, a small annoyance which had been common in Basque Country.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5376.jpeg", caption: "When I reached this lake, temperatures dropped and rain fell more heavily"}]} />
+
+As I got higher and higher, the weather changed rather dramatically. The droplets grew larger, wind picked up and it became very very cold. All of a sudden, I was shivering and I couldn't use my fingers properly to open my backpack. I knew I should have taken out my warm jacket on earlier! With some effort, I got out my puffy jacket and long pants, and I kept hiking in order to keep warm.
+
+<p>Finding shelter became my next objective. I considered going back down the slippery boulders or pitching my tent somewhere, but both ideas were bad in this rain. On my map, I saw a point about 200m ahead of my position labelled "Abri sous roche". Google Translate told me that this meant "shelter under rock". With no better option, I decided to go for it.</p>
+
+When I reached the "shelter", I discovered that it was quite literally a large rock with an overhanging ledge. Somebody had built a ring of stones around the exposed side. I saw a dry spot in the corner, exactly the right size for a person lying down. Yes! I quickly ducked under the rock, peeled off my wet clothes, took out my sleeping bag, and plopped down on the dry spot.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5382.jpeg", caption: "Hiding under a rock during the storm"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5383.jpeg", caption: "I spent a couple hours here in my sleeping bag to stay warm. It was actually very dry and sheltered from the wind."}]} />
+The storm grew heavier but I could feel my hands and feet slowly warming up. This shelter actually turned out to be pretty effective!
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5383.jpeg", caption: "I spent a couple hours here in my sleeping bag to stay warm. It was actually very dry and protected from the wind."}]} />
+
+Two hours passed by with the storm raging on. Suddenly I felt a ray of sun heat up my sleeping bag. I peered out, and saw a gap in the clouds! I was comfortable and I didn't want to move, but this was my chance. Either I had to keep going up and over the pass to reach the next refuge, or go back down the way I came before the weather changed again. A split second decision—I decided to keep going up, knowing that if things went south again I could return here and safely spend the night under my rock. I quickly packed up my things, ate a bar of chocolate and a tray of jamón, and set off.
+
+<p>Thirty minutes into my climb, I felt the sky darken again. This time it wasn't rain that fell from the sky. It was snow—I had climbed a above the freezing line now. The pass was getting close, and I had to keep going. Refuge Portillon was my goal, and it was much closer than going back down to Refuge Soula. Luckily, the snow did not feel nearly as cold as the earlier rain with wind. I had warm clothes on now, and the snow slid right off my body.</p>
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5385.jpeg", caption: "Rain turned into snow..."}]} />
 
@@ -19,13 +35,25 @@ const GR = () => (
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5391.jpeg", caption: "Getting close to the col"}]} />
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5393.jpeg", caption: "Frozen cairns showed me the way"}]} />
+
     <ImageGroup images={[{path: "/images/gr-10/IMG_5395.jpeg", caption: "The final climb"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Col des Gourgs-Blancs, 2876m"}]} />
+Following some well-placed cairns, I successfully navigated the near-whiteout to the top of the col!
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Col des Gourgs-Blancs, 2876m!"}]} />
+
+It was not over yet. I had to cross a talus field, descend down, and climb back up to the Col Pluviometre. Cairns became sparse here. Relying on my map app (bless Mapy.cz), I was able to slowly make progress to the second col, checking my phone every few minutes.
+
+<p>Luckily Col Pluviometre did not have any technical downclimbing as I had feared. The path descended through a mess of boulders, but there were cairns along the way! I stepped carefully and used my trekking poles to not slip.<p>
+
+No pictures were taken of the descent to Portillon, since it was very important to focus. An hour later at a quarter past 7, I saw the refuge appear out of the mist. I had made it!
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5399.jpeg", caption: "Made it to the refuge!"}]} />
 
-Refuge Soula to Refuge de Portillon - 13.2km and 1510m gained.
+People in the refuge stared as I walked in dripping wet. I was only 15 minutes late for dinner, and the first slurp of soup felt amazing as it warmed my entire body. This was the most definitely the hardest day of my trek so far!
+
+<p>Refuge Soula to Refuge de Portillon - 13.2km and 1510m gained.</p>
   </Post>
 );
 
