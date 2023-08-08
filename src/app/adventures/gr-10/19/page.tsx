cb56daa84b43513d4ddc7f88791a3be50dcbdab3
George Wang <wgeorge@berkeley.edu>
2023-08-08 12:19:48 +0200

    Update page.tsx 19 part 1

--- src/app/adventures/gr-10/19/page.tsx
+++ src/app/adventures/gr-10/19/page.tsx
@@ -2,45 +2,40 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Pombie (HRP Day 14)" date="July 24, 2023" prev="/adventures/gr-10/13" next="/adventures/gr-10/15">
+  <Post title="'Rest' Day (HRP Day 19)" date="July 29, 2023" prev="/adventures/gr-10/18" next="/adventures/gr-10/20">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5175.jpeg", caption: "Lake swim"}]} />
 
-<p>During the night there was a dry thunderstorm with lightning. I experienced some some the strongest winds I’d ever felt while camping. One gust was so powerful that it snapped the end of my hiking pole and my tent collapsed!</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5183.jpeg", caption: "Lake swim"}]} />
 
-I spent half of the night sleeping under a collapsed tent, because the winds were too strong for me to go outside and try to repitch it. Around 4 or 5 am a period of calmness arrived so I hacked together a new way to prop up my tent despite the broken pole. Finally I could get some proper sleep.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5184.jpeg", caption: "Lake swim"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4959.jpeg", caption: "Crazy windy campsite"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5192.jpeg", caption: "Lake swim"}]} />
 
-Luckily today was a short day, and I had booked a stay inside Refuge de Pombie. The reservation included dinner, which I was pretty excited about.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5195.jpeg", caption: "Lake swim"}]} />
 
-I began the day’s trek with a short climb to a col, which offered a great view of the famous Pic du Midi d’Ossau. This col was also the border and I reentered France soon after. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5199.jpeg", caption: "Lake swim"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4960.jpeg", caption: "Col des Moines, 2168m"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5202.jpeg", caption: "Lake swim"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4965.jpeg", caption: "Epic view of Pic du Midi d'Ossau, a Pyrenean icon"}]} />
+Today was not really a rest day because I still ended up hiking and doing a bunch of extra miles.
 
-Continuing the descent into some clouds, I met a Scottish family while trying to navigate in poor visibility. When I found the trail it was time to ascend again.
+In the morning I went to the sport shop in Gavarnie to see if I could fix my broken pole from that night with the wind. The owner told me to grab a coffee and he would replace my pole tips when I got back.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4967.jpeg", caption: "Wayfinding in the clouds"}]} />
+As soon as they were fixed I wanted to go on a test hike. I decided to hitch the 10km up to Col de Tentes and was picked up by the first car I saw. The nice driver was a microbiologist from Bordeaux.
 
-Halfway up to Col de Peyreget, I caught up to Camille, who had stayed at Refuge Ayous the previous night! She was with Jacques, a section hiker whom I’d also met previously.
+The walk up to Refuge Sarradets was easy and very busy. This was a touristy spot and it was Saturday. However the trail had some cool waterfalls and decent views.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4970.jpeg", caption: "Closer look at the Pic du Midi d'Ossau"}]} />
+After Sarradets I continued up the scree following a line of people to the Brèche de Roland. I stopped before the last bit because it was getting so crowded and the view looked good enough for me.
 
-The three of us continued the climb to the col, crossing a section of large boulders.
+As I sat there chilling, Camille appeared! She had hiked all the way up from Gavarnie. After some debating we decided to finish the last climb up to the Brèche just to see the other side, then have lunch in Refuge Sarradets.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4972.jpeg", caption: "Climbing up to Col de Peyreget"}]} />
+The descent back down to Gavarnie was not trivial because we were at 2800m. Since we didn’t have packs, we moved fast but I still felt pretty tired towards the end. 
 
-At the col we stopped to have lunch. Since we could already see the refuge from here, we were in no rush.
+Back in town, I got a text from Tristan and Tania, whom I hadn’t seen since Lescun! They arrived later that night so we all had dinner together to reunite and celebrate the section of the HRP we had just done.
 
-<p>Not interested in descending down into more clouds, Camille and I stopped to chill at a lake. I went for a swim and tried to read a french book I picked up a few days ago.</p>
+<p>Onwards to Salardu!</p>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4974.jpeg", caption: "Lake swim"}]} />
-
-The final descent to Pombie took us into the mist, which would linger for the rest of the day and night. the refuge was small, but dinner was great and the company was interesting. I met one Hungarian guy who was trying to complete the HRP in 25 days.
-
-<p>The misty weather would continue into tomorrow, but at least I could sleep under a roof tonight!</p>
-
-Ibon de Escalar to Refuge de Pombie - 9.2km and 728m gained.
+Col des Tentes to Brèche de Roland to Gavarnie - 16.98km and 771m gained.
   </Post>
 );
 
