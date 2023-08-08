d020a53350a3ce16f1895789c3ae8fa690165a71
George Wang <wgeorge@berkeley.edu>
2023-08-08 12:30:54 +0200

    Update page.tsx 19

--- src/app/adventures/gr-10/19/page.tsx
+++ src/app/adventures/gr-10/19/page.tsx
@@ -3,37 +3,39 @@ import Post from '@/components/Post';
 
 const GR = () => (
   <Post title="'Rest' Day (HRP Day 19)" date="July 29, 2023" prev="/adventures/gr-10/18" next="/adventures/gr-10/20">
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5175.jpeg", caption: "Lake swim"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5183.jpeg", caption: "La Brèche de Roland (Roland's breach), a gigantic cleft in the wall"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5183.jpeg", caption: "Lake swim"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5184.jpeg", caption: "Lake swim"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5192.jpeg", caption: "Lake swim"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5195.jpeg", caption: "Lake swim"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5199.jpeg", caption: "Lake swim"}]} />
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5202.jpeg", caption: "Lake swim"}]} />
-
-Today was not really a rest day because I still ended up hiking and doing a bunch of extra miles.
+<p>Today was not really a rest day because I still ended up hiking and doing a bunch of extra miles.</p>
 
 In the morning I went to the sport shop in Gavarnie to see if I could fix my broken pole from that night with the wind. The owner told me to grab a coffee and he would replace my pole tips when I got back.
 
-As soon as they were fixed I wanted to go on a test hike. I decided to hitch the 10km up to Col de Tentes and was picked up by the first car I saw. The nice driver was a microbiologist from Bordeaux.
+<p>As soon as they were fixed I wanted to go on a test hike. I decided to hitch the 10km up to Col de Tentes and was picked up by the first car I saw. The nice driver was a microbiologist from Bordeaux.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5175.jpeg", caption: "Hitch-hiked up to this point."}]} />
 
 The walk up to Refuge Sarradets was easy and very busy. This was a touristy spot and it was Saturday. However the trail had some cool waterfalls and decent views.
 
-After Sarradets I continued up the scree following a line of people to the Brèche de Roland. I stopped before the last bit because it was getting so crowded and the view looked good enough for me.
+<p>After Sarradets I continued up the scree following a line of people to the Brèche de Roland. I stopped before the last bit because it was getting so crowded and the view looked good enough for me.</p>
 
-As I sat there chilling, Camille appeared! She had hiked all the way up from Gavarnie. After some debating we decided to finish the last climb up to the Brèche just to see the other side, then have lunch in Refuge Sarradets.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5184.jpeg", caption: "Too cold of a lake to swim in"}]} />
 
-The descent back down to Gavarnie was not trivial because we were at 2800m. Since we didn’t have packs, we moved fast but I still felt pretty tired towards the end. 
+As I sat there chilling, Camille appeared! She had hiked all the way up from Gavarnie.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5199.jpeg", caption: "She hiked up here, I hitch-hiked!"}]} />
+
+After some debating we decided to finish the last climb up to the Brèche just to see the other side, then have lunch in Refuge Sarradets.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5195.jpeg", caption: "Camille, random orange guy, and me inside the brèche"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5192.jpeg", caption: "Brèche is pretty tall"}]} />
+
+The descent back down to Gavarnie was not trivial because we were at 2800m. Since we didn’t have packs, we moved fast but I still felt pretty tired towards the end.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5202.jpeg", caption: "Back down to Gavarnie"}]} />
 
 Back in town, I got a text from Tristan and Tania, whom I hadn’t seen since Lescun! They arrived later that night so we all had dinner together to reunite and celebrate the section of the HRP we had just done.
 
-<p>Onwards to Salardu!</p>
+<p>Tomorrow I move onward to Salardu!</p>
 
 Col des Tentes to Brèche de Roland to Gavarnie - 16.98km and 771m gained.
   </Post>
