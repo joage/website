c143197275e4aae2a1e6f6994997221a969c9343
George Wang <wgeorge@berkeley.edu>
2023-08-08 11:42:35 +0200

    Update page.tsx 17

--- src/app/adventures/gr-10/17/page.tsx
+++ src/app/adventures/gr-10/17/page.tsx
@@ -2,45 +2,35 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Pombie (HRP Day 14)" date="July 24, 2023" prev="/adventures/gr-10/13" next="/adventures/gr-10/15">
+  <Post title="Gaube (HRP Day 17)" date="July 27, 2023" prev="/adventures/gr-10/16" next="/adventures/gr-10/18">
 
-<p>During the night there was a dry thunderstorm with lightning. I experienced some some the strongest winds I’d ever felt while camping. One gust was so powerful that it snapped the end of my hiking pole and my tent collapsed!</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5114.jpeg", caption: "Col des Mulets, 2591m. Windy."}]} />
 
-I spent half of the night sleeping under a collapsed tent, because the winds were too strong for me to go outside and try to repitch it. Around 4 or 5 am a period of calmness arrived so I hacked together a new way to prop up my tent despite the broken pole. Finally I could get some proper sleep.
+Today was a short day so I slept in and had a big breakfast of mashed potatos. The signage said it would take only 5 hours to Oulettes de Gaube from Wallon.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4959.jpeg", caption: "Crazy windy campsite"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5098.jpeg", caption: "The morning climb, with a late start"}]} />
 
-Luckily today was a short day, and I had booked a stay inside Refuge de Pombie. The reservation included dinner, which I was pretty excited about.
+I began the hike a couple hours after my friends had left. 
 
-I began the day’s trek with a short climb to a col, which offered a great view of the famous Pic du Midi d’Ossau. This col was also the border and I reentered France soon after. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5101.jpeg", caption: "The way up goes through the notch in the middle of the pic"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4960.jpeg", caption: "Col des Moines, 2168m"}]} />
+The walk was lovely, and I passed some lakes before tackling the two Cols of the day. Neither felt too difficult, especially after a generous break to have lunch.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4965.jpeg", caption: "Epic view of Pic du Midi d'Ossau, a Pyrenean icon"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5107.jpeg", caption: "Between the two cols"}]} />
 
-Continuing the descent into some clouds, I met a Scottish family while trying to navigate in poor visibility. When I found the trail it was time to ascend again.
+When I crested the second col, the north face of Vignemale, an iconic Pyrenean peak came into view.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4967.jpeg", caption: "Wayfinding in the clouds"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5118.jpeg", caption: "Vignemale (3298m)!"}]} />
 
-Halfway up to Col de Peyreget, I caught up to Camille, who had stayed at Refuge Ayous the previous night! She was with Jacques, a section hiker whom I’d also met previously.
+After a 500m descent I made it to the Gaube valley, an expansive glacial meadow with a touristy refuge at the far end. 
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4970.jpeg", caption: "Closer look at the Pic du Midi d'Ossau"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5115.jpeg", caption: "Peering down into the Gaube Valley"}]} />
 
-The three of us continued the climb to the col, crossing a section of large boulders.
+The whole walk only took 4 hours, including breaks and lunch. Since there were supposed to be storms tonight, I selected a nice sheltered spot to plant my tent. I spent the rest of my day cooking and eating lots of food to lighten my pack. The next resupply point in Gavarnie was soon approaching!
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4972.jpeg", caption: "Climbing up to Col de Peyreget"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5121.jpeg", caption: "A storm approaching..."}]} />
 
-At the col we stopped to have lunch. Since we could already see the refuge from here, we were in no rush.
-
-<p>Not interested in descending down into more clouds, Camille and I stopped to chill at a lake. I went for a swim and tried to read a french book I picked up a few days ago.</p>
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4974.jpeg", caption: "Lake swim"}]} />
-
-The final descent to Pombie took us into the mist, which would linger for the rest of the day and night. the refuge was small, but dinner was great and the company was interesting. I met one Hungarian guy who was trying to complete the HRP in 25 days.
-
-<p>The misty weather would continue into tomorrow, but at least I could sleep under a roof tonight!</p>
-
-Ibon de Escalar to Refuge de Pombie - 9.2km and 728m gained.
+Refuge Wallon to Oulettes de Gaube - 9.60km and 875m gained.
   </Post>
 );
 
