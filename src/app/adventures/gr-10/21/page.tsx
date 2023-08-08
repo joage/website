5e26aa97174a85302448600f41f75fb44cff949c
George Wang <wgeorge@berkeley.edu>
2023-08-08 16:54:54 +0200

    Update page.tsx 21

--- src/app/adventures/gr-10/21/page.tsx
+++ src/app/adventures/gr-10/21/page.tsx
@@ -2,45 +2,36 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Pombie (HRP Day 14)" date="July 24, 2023" prev="/adventures/gr-10/13" next="/adventures/gr-10/15">
+  <Post title="Barroude Lakes (HRP Day 21)" date="July 31, 2023" prev="/adventures/gr-10/20" next="/adventures/gr-10/22">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5299.jpeg", caption: "Lacs de Barroude"}]} />
 
-<p>During the night there was a dry thunderstorm with lightning. I experienced some some the strongest winds I’d ever felt while camping. One gust was so powerful that it snapped the end of my hiking pole and my tent collapsed!</p>
+I began my day with a big breakfast of tortillas, fromage de brebis, and cereal bars. I intended to take a shorter day than suggested by the guidebooks, and camp at some lacs (lakes) recommended by somebody at the campground.
 
-I spent half of the night sleeping under a collapsed tent, because the winds were too strong for me to go outside and try to repitch it. Around 4 or 5 am a period of calmness arrived so I hacked together a new way to prop up my tent despite the broken pole. Finally I could get some proper sleep.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5238.jpeg", caption: "Breakfast in Héas"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4959.jpeg", caption: "Crazy windy campsite"}]} />
+Even though it was a smaller distance, the elevation gain still surpassed 1000m. Most of the day was spent climbing, and with no shade.
 
-Luckily today was a short day, and I had booked a stay inside Refuge de Pombie. The reservation included dinner, which I was pretty excited about.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5243.jpeg", caption: "The morning climb"}]} />
 
-I began the day’s trek with a short climb to a col, which offered a great view of the famous Pic du Midi d’Ossau. This col was also the border and I reentered France soon after. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5254.jpeg", caption: "The glaciated peak in the back is Vignemale, which I passed on day 17!"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4960.jpeg", caption: "Col des Moines, 2168m"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5293.jpeg", caption: "Hourquette de Héas, 2608m"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4965.jpeg", caption: "Epic view of Pic du Midi d'Ossau, a Pyrenean icon"}]} />
+After a small but steep downhill, the trail traversed a few rocky hillsides to reach the Lacs de Barroude.
 
-Continuing the descent into some clouds, I met a Scottish family while trying to navigate in poor visibility. When I found the trail it was time to ascend again.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5295.jpeg", caption: "Traverse"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4967.jpeg", caption: "Wayfinding in the clouds"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5297.jpeg", caption: "Approaching the lakes"}]} />
 
-Halfway up to Col de Peyreget, I caught up to Camille, who had stayed at Refuge Ayous the previous night! She was with Jacques, a section hiker whom I’d also met previously.
+When I arrived at the lakes, it was 2:30 and Camille was just getting ready to hike onwards to Parzan. We said goodbye here, since the lakes were so beautiful and I wanted to stay the night.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4970.jpeg", caption: "Closer look at the Pic du Midi d'Ossau"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5310.jpeg", caption: "Sheep came to visit during lunch"}]} />
 
-The three of us continued the climb to the col, crossing a section of large boulders.
+I spent the rest of the day cooking lots of polenta, lounging, and swimming. Two meals later, it was time for bed!
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4972.jpeg", caption: "Climbing up to Col de Peyreget"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5311.jpeg", caption: "Camp site"}]} />
 
-At the col we stopped to have lunch. Since we could already see the refuge from here, we were in no rush.
-
-<p>Not interested in descending down into more clouds, Camille and I stopped to chill at a lake. I went for a swim and tried to read a french book I picked up a few days ago.</p>
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4974.jpeg", caption: "Lake swim"}]} />
-
-The final descent to Pombie took us into the mist, which would linger for the rest of the day and night. the refuge was small, but dinner was great and the company was interesting. I met one Hungarian guy who was trying to complete the HRP in 25 days.
-
-<p>The misty weather would continue into tomorrow, but at least I could sleep under a roof tonight!</p>
-
-Ibon de Escalar to Refuge de Pombie - 9.2km and 728m gained.
+Héas to Lacs de Barroude - 14.07km and 1559m gained.
   </Post>
 );
 
