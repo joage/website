bda4422f8d37f3d94ba2361f1d65eb0d585f100b
George Wang <wgeorge@berkeley.edu>
2023-08-08 17:51:21 +0200

    Update page.tsx 24

--- src/app/adventures/gr-10/24/page.tsx
+++ src/app/adventures/gr-10/24/page.tsx
@@ -2,45 +2,40 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Pombie (HRP Day 14)" date="July 24, 2023" prev="/adventures/gr-10/13" next="/adventures/gr-10/15">
+  <Post title="Fog (HRP Day 24)" date="August 3, 2023" prev="/adventures/gr-10/23" next="/adventures/gr-10/25">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5359.jpeg", caption: "Going down into the brouillard after an amazingly clear morning"}]} />
 
-<p>During the night there was a dry thunderstorm with lightning. I experienced some some the strongest winds I’d ever felt while camping. One gust was so powerful that it snapped the end of my hiking pole and my tent collapsed!</p>
+    The next four passes on the HRP were going to be steep and progressively more technical. I woke up this morning and left late, but excited for the challenge.
 
-I spent half of the night sleeping under a collapsed tent, because the winds were too strong for me to go outside and try to repitch it. Around 4 or 5 am a period of calmness arrived so I hacked together a new way to prop up my tent despite the broken pole. Finally I could get some proper sleep.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5345.jpeg", caption: "Nice morning walk with a pastoral backdrop"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4959.jpeg", caption: "Crazy windy campsite"}]} />
+At a river crossing, my map showed the HRP leaving the GR 11 on a faint trail. I took a break at the junction and refilled my water bottle in the stream to prepare for the climb.
 
-Luckily today was a short day, and I had booked a stay inside Refuge de Pombie. The reservation included dinner, which I was pretty excited about.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5348.jpeg", caption: "Splitting off from the GR 11 to go up this valley"}]} />
 
-I began the day’s trek with a short climb to a col, which offered a great view of the famous Pic du Midi d’Ossau. This col was also the border and I reentered France soon after. 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5349.jpeg", caption: "Trail was hard to find here"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4960.jpeg", caption: "Col des Moines, 2168m"}]} />
+The trail got steeper and transitioned from grass to reddish colored scree.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4965.jpeg", caption: "Epic view of Pic du Midi d'Ossau, a Pyrenean icon"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5353.jpeg", caption: "The crumbly climb to the col"}]} />
 
-Continuing the descent into some clouds, I met a Scottish family while trying to navigate in poor visibility. When I found the trail it was time to ascend again.
+I made it to the col, where clouds rolled over from France into Spain.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4967.jpeg", caption: "Wayfinding in the clouds"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5356.jpeg", caption: "Col d'Aygues Tortes, 2687m"}]} />
 
-Halfway up to Col de Peyreget, I caught up to Camille, who had stayed at Refuge Ayous the previous night! She was with Jacques, a section hiker whom I’d also met previously.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5361.jpeg", caption: "Lunch"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4970.jpeg", caption: "Closer look at the Pic du Midi d'Ossau"}]} />
+At the col I met a french couple, Jade and Thivo, who were also doing the HRP. We descended into the mist together.
 
-The three of us continued the climb to the col, crossing a section of large boulders.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5363.jpeg", caption: "The descent"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4972.jpeg", caption: "Climbing up to Col de Peyreget"}]} />
+The fog was not too hard to navigate, but as we got lower it turned into rain.
 
-At the col we stopped to have lunch. Since we could already see the refuge from here, we were in no rush.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5365.jpeg", caption: "Passing an area of many streams, Prat-Caseneuve"}, {path: "/images/gr-10/IMG_5369.jpeg", caption: "Overgrown trails get your legs wet when it rains"}]} />
 
-<p>Not interested in descending down into more clouds, Camille and I stopped to chill at a lake. I went for a swim and tried to read a french book I picked up a few days ago.</p>
+I stopped to rest in a cabin at Prat-Caseneuve where I initially planned to camp. But because it was still early and the weather was bad I continued on to Refuge Soula so I could dry off.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4974.jpeg", caption: "Lake swim"}]} />
-
-The final descent to Pombie took us into the mist, which would linger for the rest of the day and night. the refuge was small, but dinner was great and the company was interesting. I met one Hungarian guy who was trying to complete the HRP in 25 days.
-
-<p>The misty weather would continue into tomorrow, but at least I could sleep under a roof tonight!</p>
-
-Ibon de Escalar to Refuge de Pombie - 9.2km and 728m gained.
+Camping El Forcallo to Refuge Soula - 16.32km and 1186m gained.
   </Post>
 );
 
