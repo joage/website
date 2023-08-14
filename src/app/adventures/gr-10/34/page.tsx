6370abf773f3d677731c75a5965505cac0ec16ff
George Wang <wgeorge@berkeley.edu>
2023-08-14 17:30:02 +0200

    Update page.tsx 34 pics

--- src/app/adventures/gr-10/34/page.tsx
+++ src/app/adventures/gr-10/34/page.tsx
@@ -4,29 +4,33 @@ import Post from '@/components/Post';
 const GR = () => (
   <Post title="Andorra (HRP Day 34)" date="August 13, 2023" prev="/adventures/gr-10/33" next="/adventures/gr-10/35">
 
-I only had one goal in mind for today-to swim in a lake. My body needed a break from the past few days of high cols and difficult hiking, so I kept the mileage short in order to relax.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5669.jpeg", caption: "Leaving Val Ferrera"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5434.jpeg", caption: "The morning descent"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5670.jpeg", caption: "The morning climb"}]} />
 
-By the road in Espitau de Vielha, I ran into Dan and Benjamin, two HRP hikers who had done the same itinerary as me yesterday. They shared my desire to bathe in cold water today.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5676.jpeg", caption: "Approaching some lakes"}]} />
 
-<p>After I ate a big lunch, I climbed a gentle slope following the GR 11. It was not a difficult trail but I still felt tired from yesterday.</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5678.jpeg", caption: "Horses and the shadow of the mountain"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5435.jpeg", caption: "Back on the GR 11"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5682.jpeg", caption: "Climbing the crumbly col"}]} />
 
-I reached the Lake Rius by mid-afternoon, so I had plenty of time to swim and lay in the sun. I may have stayed a bit too long because the next day I felt a slight sunburn on my chest.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5684.jpeg", caption: "Steep section near the top"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5436.jpeg", caption: "Lac de Rius"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_dhdhebe.jpeg", caption: "Lakes far below us"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5437.jpeg", caption: "Swim spot"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5687.jpeg", caption: "Port de Baiau, 2757m. Enterring Andorra!"}]} />
 
-For the final part of the day, I descended 7 km along the GR 11 and climbed a short but steep hill to my destination, Refugi Restanca by a dammed lake. Here I ran into Benjamin and Dan again, so we had a drink. They left to bivouac somewhere, and I decided to stay in the refuge. I spent the rest of the day cooking all of my food and eating way too much mashed potatoes.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5690.jpeg", caption: "Top of Coma Pedrosa, looking down the ridge"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5445.jpeg", caption: "Dinner"}, {path: "/images/gr-10/IMG_5448.jpeg", caption: "He wanted some cheese"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5694.jpeg", caption: "Pic de Coma Pedrosa, 2943m"}]} />
 
-Tomorrow I will be in Salardú, a town and resupply point!
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5697.jpeg", caption: "Looking back on Spain"}]} />
 
-<p>Refugi Mulleres to Refugi Restanca - 17.0km and 1045m gained.</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5698.jpeg", caption: "Lake swim, not too cold. Pebbly shore felt like a beach."}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5702.jpeg", caption: "Toilets at the refuge"}]} />
+
+<p>Refugi de Val Ferrera to Pic de Comapedrosa to Refugi de Comapedrosa - 14.01km and 1347m gained.</p>
   </Post>
 );
 
