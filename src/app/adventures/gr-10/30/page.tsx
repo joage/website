d188c42e01e4355691facb2b5615fe848b90829a
George Wang <wgeorge@berkeley.edu>
2023-08-14 15:54:04 +0200

    Update page.tsx 30

--- src/app/adventures/gr-10/30/page.tsx
+++ src/app/adventures/gr-10/30/page.tsx
@@ -3,30 +3,59 @@ import Post from '@/components/Post';
 
 const GR = () => (
   <Post title="Boulders (HRP Day 30)" date="August 9, 2023" prev="/adventures/gr-10/28" next="/adventures/gr-10/31">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5499.jpeg", caption: "Tuc de Marimanha 2679m"}]} />
 
-I only had one goal in mind for today-to swim in a lake. My body needed a break from the past few days of high cols and difficult hiking, so I kept the mileage short in order to relax.
+This morning I left the campsite in Arties at 9am. My bag felt extremely heavy from the food I bought in Vielha. Walking along the road, I tried to get a hitch but there were no takers at first. After a few hundred meters, a guy named Jose picked me up. As it turned out, he was going to the exact same place I wanted to go, Baqueira! In fact, he was even heading up the small side road that I planned on walking along.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5434.jpeg", caption: "The morning descent"}]} />
+<p>Jose saved me about 600m of ascent today. It kind of felt like cheating, but today’s hike was going to be a hard one so I was grateful.</p>
 
-By the road in Espitau de Vielha, I ran into Dan and Benjamin, two HRP hikers who had done the same itinerary as me yesterday. They shared my desire to bathe in cold water today.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5499.jpeg", caption: "Road leading to the ski resort Baqueira"}]} />
 
-<p>After I ate a big lunch, I climbed a gentle slope following the GR 11. It was not a difficult trail but I still felt tired from yesterday.</p>
+After I was dropped off near the ski lifts at Baquiera, I started the climb up the valley to some lakes. My legs felt heavy and I had eaten so much in the past couple days that my pace was significantly slower.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5435.jpeg", caption: "Back on the GR 11"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5481.jpeg", caption: "Back on the trail!"}]} />
 
-I reached the Lake Rius by mid-afternoon, so I had plenty of time to swim and lay in the sun. I may have stayed a bit too long because the next day I felt a slight sunburn on my chest.
+Once I got to the lakes I could see Aneto, the highest peak in the Pyrenees far behind me. It was such a clear day! I also a group of isards, a type of Pyrenean antelope.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5436.jpeg", caption: "Lac de Rius"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5483.jpeg", caption: "Tuc de Marimanha from the meadow"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5437.jpeg", caption: "Swim spot"}]} />
+My first objective was to climb the Tuc de Marimanha. This was a cool climb because there was no trail, just a steep climb up the west face. Boulders gave way to grassy ledges, and at one point the trail was so steep I could smell the flowers and herbs without even bending down.
 
-For the final part of the day, I descended 7 km along the GR 11 and climbed a short but steep hill to my destination, Refugi Restanca by a dammed lake. Here I ran into Benjamin and Dan again, so we had a drink. They left to bivouac somewhere, and I decided to stay in the refuge. I spent the rest of the day cooking all of my food and eating way too much mashed potatoes.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5491.jpeg", caption: "Initial bouldery section"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5445.jpeg", caption: "Dinner"}, {path: "/images/gr-10/IMG_5448.jpeg", caption: "He wanted some cheese"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5499.jpeg", caption: "Nearing the summit. Pic d'Aneto is visible far in the distance"}]} />
 
-Tomorrow I will be in Salardú, a town and resupply point!
+At the top of Marimanha I met a Catalonian couple and we took pictures for each other.
 
-<p>Refugi Mulleres to Refugi Restanca - 17.0km and 1045m gained.</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5509.jpeg", caption: "Summit!"}, {path: "/images/gr-10/IMG_5504.jpeg", caption: "Picture was requested in the middle of lunch"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5402.jpeg", caption: "Looking east from the top"}]} />
+
+From the summit a ridge walk led to some smaller peaks, and into a boulder filled valley.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5512.jpeg", caption: "Ridge walk"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5514.jpeg", caption: "High alpine lakes"}]} />
+
+The boulders started out fun, but quickly became the most annoying part of the entire HRP for me. The path did not follow an obvious line. Instead it traversed a steep, unstable hillside where progress slowed down.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5516.jpeg", caption: "Terrible boulder traverse that went on forever."}]} />
+
+I endured a few grueling hours of this, until I finally reached a path at a col! It felt great to walk on a trail again.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5519.jpeg", caption: "Collada del Clot de Moredo, 2430m. Survived the boulders."}]} />
+
+I descended towards Alos d’Isil, where I had booked dinner and a gîte for the night. This valley was drier than all the ones I’ve seen before, and it reminded me of hiking back home in California. I descended about 700m before finding a water source.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5522.jpeg", caption: "A dry valley on the way down."}]} />
+
+When I arrived at the refugi in Alos d’Isil, I discovered that there was nobody else staying there that night. Since it was only me having dinner, I sat with the hosts, Marta and Aniol. They had recently moved into this tiny village. They served a soup and a delicious Catalan dish, Botifarra with mongetes.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5523.jpeg", caption: "Alos d'Isil"}]} />
+
+Alos d’Isil had no shops whatsoever, and only 6 people lived here year-round.  However the refugi was a great place to stay after a hard day of hiking!
+
+<p>Baqueira to Alos d'Isil - 17.85km and 1248m gained.</p>
   </Post>
 );
 
