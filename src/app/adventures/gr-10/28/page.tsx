f5ffe908a346b6eb9e88a8e2ddf3b03be80a5b79
George Wang <georgeyw@gmail.com>
2023-08-14 15:15:41 +0200

    Update page.tsx 28 29

--- src/app/adventures/gr-10/28/page.tsx
+++ src/app/adventures/gr-10/28/page.tsx
@@ -2,13 +2,13 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Arties (HRP Day 28)" date="August 7, 2023" prev="/adventures/gr-10/27">
+  <Post title="Arties (HRP Day 28-29)" date="August 7, 2023" prev="/adventures/gr-10/27" next="/adventures/gr-10/30">
 
-    Today I took a direct path down a road to Arties, where I pited my tent at a campground. Here I wanted to spend two nights to rest before the next section of the HRP.
+    Today I took a direct path down a road to Arties, where I pitched my tent at a campground. Here I wanted to spend two nights to rest before the next section of the HRP.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5452.jpeg", caption: "Arties, Spain"}]} />
 
-Arties is located in between Salardú and Vielha, so I could easily hitchhike or take a bus between the three towns. Here are some pics from my rest day.
+Arties is located in between Salardú and Vielha, so I could easily hitchhike or take a bus between the three towns. Here are some pics from my rest days.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5455.jpeg", caption: ""}]} />
 
@@ -16,8 +16,10 @@ Arties is located in between Salardú and Vielha, so I could easily hitchhike or
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5462.jpeg", caption: "I had dinner with Benjamin and Dan."}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5464.jpeg", caption: "Then we had a second dinner because we were still hungry."}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5464.jpeg", caption: "We had a second dinner because we were still hungry."}]} />
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5470.jpeg", caption: "I hitched to Vielha the next day. Got groceries and a haircut."}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5473.jpeg", caption: "Apple tart"}]} />
 Refugi Restanca to Arties - 10.01km and 864m descended.
   </Post>
 );
