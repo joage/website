6c8946219067ef6988e5d3cc3578bacc55df2c2a
George Wang <wgeorge@berkeley.edu>
2023-08-14 23:17:57 +0200

    edit day 34

--- src/app/adventures/gr-10/33/page.tsx
+++ src/app/adventures/gr-10/33/page.tsx
@@ -3,9 +3,12 @@ import Post from '@/components/Post';
 
 const GR = () => (
   <Post title="Val Ferrera (HRP Day 33)" date="August 12, 2023" prev="/adventures/gr-10/32" next="/adventures/gr-10/34">
+
+Although the last week has had really amazing scenery, I was missing some social contact due to walking alone. I looked forward to talking to people at the refuge I booked tonight at Val Ferrera.
+
     <ImageGroup images={[{path: "/images/gr-10/IMG_5636.jpeg", caption: "Alpenglow on the nearby peaks"}]} />
 
-I got up pretty early today to avoid walking in the sun. The morning climb looked better and felt easier in the shade.
+I got up pretty early today to avoid walking in the sun. The morning climbs look better and feel much easier in the shade. 
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5641.jpeg", caption: "Starting the climb in the shade for once"}]} />
 
@@ -33,7 +36,7 @@ Some time later, I continued down the valley to a stream. I dipped my feet in be
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5665.jpeg", caption: "View from Refugi de Val Ferrera"}]} />
 
-At the refuge, I ate dinner and met some other hikers, including one guy who was doing the HRP but also climbing a bunch of peaks as side quests. He did 3000m of climbing today, which is insane.
+At the refuge, I ate dinner and met some other hikers, including one Spanish guy who was doing the HRP but also climbing a bunch of peaks as side quests. He did 3000m of climbing today, which is insane.
 
 <p>Pla de Boavi to Refugi de Val Ferrera - 13.47km and 1297m gained.</p>
   </Post>
