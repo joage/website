ff4a2f72faa2008abb976acb3f54a7dcaada37b1
George Wang <wgeorge@berkeley.edu>
2023-08-17 13:29:17 +0200

    Update page.tsx 36

--- src/app/adventures/gr-10/36/page.tsx
+++ src/app/adventures/gr-10/36/page.tsx
@@ -2,47 +2,49 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Andorra (HRP Day 34)" date="August 13, 2023" prev="/adventures/gr-10/33" next="/adventures/gr-10/35">
+  <Post title="Oval (HRP Day 36)" date="August 15, 2023" prev="/adventures/gr-10/35" next="/adventures/gr-10/37">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5687.jpeg", caption: "Port de Baiau, 2757m. Enterring Andorra!"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5749.jpeg", caption: "The perfect oval"}]} />
 
-Today I left Val Ferrera with Pavol, a Slovakian guy I met at the refuge. He was going the same direction as me, into Andorra 🇦🇩!
+During the night a party took place in the square just outside my hostel in La Massana. Nobody in my room could get any sleep until after the thumping music subsided at 3 am.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5670.jpeg", caption: "The morning climb"}]} />
+<p>In the morning, I found out that yesterday was the annual fiesta for the village, and I happened to visit this otherwise quiet town on the exact day of its big event!</p>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5676.jpeg", caption: "Approaching some lakes"}]} />
+After I had some espresso, made myself an eggy breakfast, and packed a lunch, I was ready to head out into the mountains once more. I took a bus to El Serrat, and started my walk around 12:30.
 
-The climb took us up the valley to some lakes where horses grazed.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5725.jpeg", caption: "Departing from El Serrat bus stop"}, {path: "/images/gr-10/IMG_5731.jpeg", caption: "I passed an interpretive garden"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5678.jpeg", caption: "Horses and the shadow of the mountain"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5733.jpeg", caption: "Around 30 minutes in, I saw one of the fanciest staffed refuges I’ve seen so far."}]} />
 
-From the lakes we could see the steep path leading up to the col and over the border.
+The afternoon climb felt incredibly sluggish. I always go slower after spending some time in a town, but today was especially lazy because of my lack of sleep last night. Thankfully a constant breeze kept me from overheating in the direct sunlight.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5682.jpeg", caption: "Climbing the crumbly col"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5739.jpeg", caption: "Approaching the col"}]} />
 
-The last part of the climb was some of the steepest scree I've ever walked on.
+Near the top of the col, I saw some horses. Andorra has been weirdly free of farm animals compared to France and Spain, so this was a surprise.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5684.jpeg", caption: "Steep section near the top"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5737.jpeg", caption: "Andorran horses"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/8cf1fb5c-92df-4e86-b0d3-70684506ea66.jpeg", caption: "Crossing into Andorra at Port de Baiau"}]} />
+There is really good LTE phone signal in this country. I stayed connected throughout the day, thanks to the blessing and curse that is Andorra Telecom.
 
-After we crossed the border, I dropped my pack to climb up to Coma Pedrosa, the tallest peak in Andorra. Since it was Sunday, there was a crowd of people at the summit.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5743.jpeg", caption: "The valley I just ascended"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5690.jpeg", caption: "Top of Coma Pedrosa, looking down the ridge"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5745.jpeg", caption: "Collada de Meners, 2700m"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5694.jpeg", caption: "Pic de Coma Pedrosa, 2943m"}]} />
+A magnificent view greeted me at the col, but it was windy. I descended to a perfectly oval lake to have lunch and a swim. My paté-filled baguette was still fresh, and the water did not feel too cold!
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5697.jpeg", caption: "Looking back on Spain"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5749.jpeg", caption: ""}]} />
 
-I took some pictures and met up with Pavol again at the col. We descended to a lake with a perfect spot for swimming.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5750.jpeg", caption: "Top notch swim spot"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5698.jpeg", caption: "Lake swim, not too cold. Pebbly shore felt like a beach."}]} />
+I took my time at the lake, for I had just decided to end my day only a few kilometers away, near some streams. Around 5 pm I got back on my feet to finish the hike.
 
-We made it down to the refuge Comapedrosa early. I had a snack and a beer, and cooked two meals while waiting for the sun to go down. On the HRP, when I'm not walking, I'm most likely eating. Tomorrow will be a day in town for a resupply on food and camping gas (and probably some beers). I will spend 3 days crossing Andorra, and the first day's scenery has already set a high bar for the next two to come!
+<p>When I got to my intended camp spot, it was filled with people. A big group of multiple families had also decided to camp here, but the area was big and I chose an island by a small lake to pitch my tent.</p>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5702.jpeg", caption: "Toilets at the refuge"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5752.jpeg", caption: "Dinner"}, {path: "/images/gr-10/IMG_5756.jpeg", caption: "Sunset"}]} />
 
-<p>Refugi de Val Ferrera to Pic de Comapedrosa to Refugi de Comapedrosa - 14.01km and 1347m gained.</p>
+The Andorrans are so lucky to have such easy access to beautiful mountains!
+
+<p>El Serrat to Coms de Jan - 11.55km and 1188m gained.</p>
   </Post>
 );
 
