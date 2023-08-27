e51b16c92ad09abf090fe3290e043f281a247352
George Wang <georgeyw@gmail.com>
2023-08-27 11:40:58 +0200

    Update page.tsx 37

--- src/app/adventures/gr-10/37/page.tsx
+++ src/app/adventures/gr-10/37/page.tsx
@@ -2,7 +2,7 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36">
+  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36" next="/adventures/gr-10/38">
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
 
