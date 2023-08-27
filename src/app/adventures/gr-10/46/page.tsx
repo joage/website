fc8369159c9903601e8d89e75bcd67b67482bf7b
George Wang <wgeorge@berkeley.edu>
2023-08-27 21:30:07 +0200

    Update page.tsx 46 initial

--- src/app/adventures/gr-10/46/page.tsx
+++ src/app/adventures/gr-10/46/page.tsx
@@ -2,14 +2,14 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36" next="/adventures/gr-10/38">
+  <Post title="Le Perthus (HRP Day 46)" date="August 25, 2023" prev="/adventures/gr-10/45" next="/adventures/gr-10/47">
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
 
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
 
-<p>Coms de Jan to L'Hospitalet-près-l'Andorre - 21.37km and 1369m gained, 1984m lost</p>
+<p>Border marker 564 to Pic des Quatre Termes - 27.18km and 1263m gained</p>
   </Post>
 );
 
