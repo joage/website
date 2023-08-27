25fc4bc24524d5e8e0595175c65cc87cc07a7f9f
George Wang <georgeyw@gmail.com>
2023-08-27 16:40:16 +0200

    banyuls (#16)

---
+++ src/app/adventures/gr-10/41/page.tsx
@@ -0,0 +1,77 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
+
+const GR = () => (
+  <Post title="Epic ridge (HRP Day 41)" date="August 20, 2023" prev="/adventures/gr-10/40" next="/adventures/gr-10/42">
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5906.jpeg", caption: "Ridge walk"}]} />
+
+I woke up to footsteps outside of my tent. Last night I had set up camp in the dark, so I picked a spot super close to the trail. 
+
+<p>When I looked outside, I saw the two Belgians approaching! I keep running into these guys. Apparently, they had seen me hike past their tent last night.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5893.jpeg", caption: "Where I woke up"}]} />
+
+Today I planned to walk the border ridgeline to Refugi Ull de Ter and hit a few peaks along the way.
+
+<p>The morning climb began in the shade. I was worried I would crash again, like my climb out of L’Hospitalet two days ago, but today I felt a bit more energized after a breakfast of bread, peanut butter, and sheep’s cheese.</p>
+
+After a couple stops to refill my water at a stream and put on sunscreen, I continued the climb up the Vallée d’Eyne. Near the top of the col, I saw a group of 5 or 6 isards leaping across the trail.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5896.jpeg", caption: "The valley widened out near the top"}]} />
+
+I made it to the col after 1.5 hours. It was tiring, but I felt much better than two days ago.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5899.jpeg", caption: "Pic d'Eyne, 2789m"}]} />
+
+The next section took me over a spectacular ridge spanning 5 grassy peaks. I had the option of summiting each peak as I passed, so I climbed up 3 of them. All along this ridge I had 360 degree views of the surrounding mountains and valleys.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5903.jpeg", caption: "Pic de les Nou Fonts, 2861m"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5911.jpeg", caption: "Views from the ridge"}]} />
+
+I passed by quite a few people today, and since I was walking along the border of France and Catalonia, I needed to pick a language to say hi to people. Pretty soon I got good at guessing the right language. I noticed that many of the day hikers/runners spoke spanish and catalan, and they traveled in big groups.  The backpackers who looked a bit shy were most likely french.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5913.jpeg", caption: ""}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5914.jpeg", caption: ""}]} />
+
+The last peak in the chain, Pic de la Vaca, had white rocks as its unique feature.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5916.jpeg", caption: "Pic de la Vaca 2826m"}]} />
+
+By the time I reached Pic de la Vaca, clouds had formed overhead. There were supposed to be some thunderstorms this afternoon. 
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5926.jpeg", caption: "Freser valley"}]} />
+
+I descended quickly down a valley to the river Freser, and immediately ascended back up to a col. Then, following a line of sunday hikers, I descended down to the Refuge d’Ull de Ter. It was only 2:30.
+
+<p>I got a drink and chatted with two French guys from Paris who were going in my direction on the HRP. Water was going to be scarce for the next 25 km.</p>
+
+After they left I realized I had phone signal and spotted a food truck on google maps about 1.5 km away on the trail. I asked someone who worked at the refuge and he confirmed that the food truck was open. I grabbed my bag excitedly and left Ull de Ter.
+
+<p>The food truck was located at a ski resort, and the HRP conveniently passed right through it.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5928.jpeg", caption: "I got a burger and a tray of patatas bravas served with a slightly spicy aioli at Vallter 2000"}]} />
+
+As I ate, the clouds overhead grew darker and darker. It was necessary to  find a place to pitch my tent soon. I hiked out of the ski resort. At a stream, I filled up all my bottles because this was the last water source until Mariailles, which I would reach tomorrow afternoon.
+
+<p>With my bag heavily loaded, I walked up to a col with a lot of flat land for camping. On the way up, I kept watching the darkened sky and hoping that it wouldn’t start to rain.</p>
+
+I set up my tent and threw all my gear inside. I laid down inside and waited, but the storm never came. I could see the clouds in the distance drooping down and connecting with the land. At one point I heard thunder but it was far away. Not a single drop of rain hit my tent today.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5931.jpeg", caption: "Pla de coma Ermada"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5933.jpeg", caption: "Dinner ingredients"}, {path: "/images/gr-10/IMG_5934.jpeg", caption: "Dinner!"}]} />
+
+After I made dinner, the sun came out again for a brief moment before setting behind the mountains. I went for a short walk to get a view of Pic de Canigou then returned to my tent to sleep.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5935.jpeg", caption: "Camping on a col with all my water"}]} />
+
+Today had some really cool scenery and confusing weather. I realized that most day hikers in the Pyrenees were not as concerned about the potential storms as I was, because I saw a bunch of families with little kids continue to hike up even as the sky grew dark. 
+
+<p>Vallée d'Eyne to Pla de Coma Ermada - 18.90km and 1414m gained</p>
+  </Post>
+);
+
+export default GR;
