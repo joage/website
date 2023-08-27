aa37c8abe42d82eec035b17b60df5a84bfc88699
George Wang <wgeorge@berkeley.edu>
2023-08-27 23:04:42 +0200

    Update page.tsx 46 init

--- src/app/adventures/gr-10/46/page.tsx
+++ src/app/adventures/gr-10/46/page.tsx
@@ -2,12 +2,33 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Le Perthus (HRP Day 46)" date="August 25, 2023" prev="/adventures/gr-10/45" next="/adventures/gr-10/47">
+  <Post title="Gourmet (HRP Day 46)" date="August 25, 2023" prev="/adventures/gr-10/45" next="/adventures/gr-10/47">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+A day of eating well is how I'd describe today. It feels like vacation mode now. 
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6116.jpeg", caption: "Morning descent to Le Perthus"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6124.jpeg", caption: "An old roman building"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_2252.jpeg", caption: "You can climb inside it"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_2268.jpeg", caption: "Kebab wrap at Snack Al Paso in Le Perthus"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6151.jpeg", caption: "Meal at Col d'Ouillat"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6155.jpeg", caption: "Forest above Col d'Ouillat"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6158.jpeg", caption: "Ascending Pic Neulos"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6162.jpeg", caption: "Pic Neulos, 1257m"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6165.jpeg", caption: "Banyuls makes its first appearance on the signposts!"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6169.jpeg", caption: "Morning descent to Le Perthus"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6172.jpeg", caption: "Cooking dinner"}, {path: "/images/gr-10/IMG_6174.jpeg", caption: "Chorizo, zucchini, couscous, tapenade, harissa, bread"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6177.jpeg", caption: "And wine"}]} />
 
 <p>Border marker 564 to Pic des Quatre Termes - 27.18km and 1263m gained</p>
   </Post>
