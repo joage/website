ae4d9be5f24980b7ad9316ee6840b06bd1d3f5c9
George Wang <wgeorge@berkeley.edu>
2023-08-27 20:56:22 +0200

    Update page.tsx 44 start

--- src/app/adventures/gr-10/45/page.tsx
+++ src/app/adventures/gr-10/45/page.tsx
@@ -2,14 +2,21 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36" next="/adventures/gr-10/38">
+  <Post title="Night hike (HRP Day 45)" date="August 24, 2023" prev="/adventures/gr-10/44" next="/adventures/gr-10/46">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6091.jpeg", caption: "Making eggs for breakfast"}]} />
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6094.jpeg", caption: "Climbing to Roc de Frausa"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_2205.jpeg", caption: "Roc de Frausa, 1421m"}]} />
 
-<p>Coms de Jan to L'Hospitalet-près-l'Andorre - 21.37km and 1369m gained, 1984m lost</p>
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6101.jpeg", caption: "Forest walk. This is the last time the HRP crosses into Spain/Catalonia!"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6105.jpeg", caption: "Lunch at Hostal dels Trabucayres"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_2234.jpeg", caption: "Starting the night hike at 10pm"}]} />
+
+<p>Mas de la Fargassa to Border marker 564 - 26.19km and 1169m gained</p>
   </Post>
 );
 
