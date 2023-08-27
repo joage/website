5cce9bd0ab155a3a6ea49063ec0ba9362d2f93d5
George Wang <wgeorge@berkeley.edu>
2023-08-27 14:11:12 +0200

    Update page.tsx 39

--- src/app/adventures/gr-10/39/page.tsx
+++ src/app/adventures/gr-10/39/page.tsx
@@ -2,12 +2,61 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36" next="/adventures/gr-10/38">
+  <Post title="Tired (HRP Day 39)" date="August 18, 2023" prev="/adventures/gr-10/38" next="/adventures/gr-10/40">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5847.jpeg", caption: "How I felt today"}]} />
 
+<p>I was so tired today.</p>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+In the morning I got a pastry and a curry chicken suedois from the bakery Fournil de Louise in L’Hospitalet. There I met a talkative Dutch guy who was beginning his HRP westbound trek today, having just gotten off the train. I wish I had his energy as I climbed out of town on the steep forested hillside.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5840.jpeg", caption: "Climbing out of L'Hospitalet"}]} />
+
+I went so slowly that many families passed me. Even after I stopped to eat my lunch early, I still felt sluggish. The fact that I could constantly hear the cars on the road down in the Ariège valley reminded me that I wasn’t making a lot of progress.
+
+<p>Eventually I felt so exhausted and sleepy that I laid down on a large flat rock to take a nap. It was not my day, but at least I wasn’t in a rush to get anywhere.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5841.jpeg", caption: "A nap at 10am"}]} />
+
+Around noon I woke up and continued to the climb, still feeling slow.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5843.jpeg", caption: "Lac des Besines"}]} />
+
+Finally after 5 hours I made it to the Refuge des Besines, situated above a narrow lake. I was going to get a coffee, but fearing the eventual crash I opted for an omelette instead. This helped a bit on the next part of the climb.
+
+<p>Two Belgians and a Hungarian guy arrived as I was finishing my food. Since their english was better then their french, we got along. I left before them to continue climbing to the col.</p>
+
+I had some more energy now, and the trail followed the GR 10 so it was pretty easy. I still went super slowly on the steep parts.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5844.jpeg", caption: "Tired but in a beautiful place"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5845.jpeg", caption: "Getting close to the col"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5848.jpeg", caption: "At last, the end of the horrible climb!"}]} />
+
+As I dropped down to the valley with the gigantic Lac de Lanoux, I considered my options for tomorrow. Climbing Pic de Carlit was an option, but I felt so tired today that it didn’t feel good to attempt it, as epic as it looked from the col.
+
+<p>After I passed a shepherd herding his flock, I reached a small cabin where a some hikers, all French, had set up camp. I asked them where they had gotten water, and they directed me to a trickling stream. I sat for a while next to a group of horses and did some route planning on my phone.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5852.jpeg", caption: "I wondered why the horses kept sniffing each other’s butts in a line, but eventually I realized they were flicking the annoying flies off each other’s eyes with their tails! Very cooperative, but unless it’s a circle, the first horse never gets any benefit from this scheme."}]} />
+
+After some time, I decided on a plan. I would not do Carlit the next day, and instead take the GR 10 over a small col.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5854.jpeg", caption: "Back on the GR 10"}]} />
+
+This final climb felt okay, maybe because it wasn’t so hot. I played music from my phone as the sun passed in and out of clouds.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5855.jpeg", caption: "Last col of the day"}]} />
+
+After the col, I entered a new valley. As usual, horses were present near the top, but as I descended further, cows replaced them.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5857.jpeg", caption: "A new valley"}]} />
+
+At a marshy plateau, I saw some families with tents set up along a network of streams. I found a nice peninsula nearby to pitch my own tent. There was water on three sides like a moat so hopefully no cows could disturb me at night.
+
+<p>As the sun set, I cooked dinner. It was a long day, but it was ending on a good note! I felt progressively better during the last few kms of the hike.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5862.jpeg", caption: "Dinner"}]} />
 
 <p>Coms de Jan to L'Hospitalet-près-l'Andorre - 21.37km and 1369m gained, 1984m lost</p>
   </Post>
