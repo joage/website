d61fa9b7a146bf96686560a883910202a79c6ea1
George Wang <wgeorge@berkeley.edu>
2023-08-27 16:14:26 +0200

    Update page.tsx 42

--- src/app/adventures/gr-10/42/page.tsx
+++ src/app/adventures/gr-10/42/page.tsx
@@ -2,14 +2,93 @@ import ImageGroup from '@/components/ImageGroup';
 import Post from '@/components/Post';
 
 const GR = () => (
-  <Post title="Leaving Andorra (HRP Day 37)" date="August 16, 2023" prev="/adventures/gr-10/36" next="/adventures/gr-10/38">
+  <Post title="Canigou (HRP Day 42)" date="August 21, 2023" prev="/adventures/gr-10/41">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5948.jpeg", caption: "Morning colors"}]} />
 
+I woke up early and unzipped my tent to see a beautiful alpenglow on the peaks to my west.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+<p>As I got up to pee, I saw some isards grazing at the other end of my col. As I kept watching, more of them appeared far in the distance, an entire herd! There must have been at least a hundred.</p>
 
-<p>Coms de Jan to L'Hospitalet-près-l'Andorre - 21.37km and 1369m gained, 1984m lost</p>
+Today I planned to get as close as possible to Pic de Canigou, an iconic Occitanie/Catalan peak, and then climb it the next day. However as the day went on my plans changed drastically.
+
+<p>After a long breakfast I set off. The day began with more traversing tall grassy ridgelines. I met a group of friendly hikers from spain as I hiked along the crête leading to Canigou.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5957.jpeg", caption: "Caught up to the elusive isards"}]} />
+
+When I passed a section of more narrow trail, I saw the rocks turn from grey to a striking white color.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5961.jpeg", caption: "White and green rocks"}]} />
+
+The kilometers flew by this morning, since the trail was pretty flat. I caught up to a shepherd with her three dogs. She explained that she had about 100 sheep on the other side of the hill we were walking on. When I asked if she had any cheese to sell, she said that all her sheep were young (lambs), and were raised for their meat, not their milk! I pondered my own consumption of lamb for a bit, after we parted ways.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5963.jpeg", caption: "Pla Guillem, where the normal trail turns left to go to Mariailles but the high path continues straight ahead over the peaks"}]} />
+
+At the Pla Guillem, I was supposed to turn left to go to Refuge des Mariailles, but as I routed my track on Mapy.cz, I realized there was a “shorter” route continuing on the ridge line toward Canigou. Just then, a guy approached from the direction of Mariailles, vlogging with his camera. It turned out that he was a youtuber from Barcelona. We chatted and he confirmed my belief that Mariailles was too out of the way. Since I still had plenty of water remaining. I left the HRP here to try this shorter, higher route.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5966.jpeg", caption: "Ascending Pic des Sept Hommes"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5969.jpeg", caption: "Pic des Sept Hommes, 2651m"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5976.jpeg", caption: "A lone isard on the hill"}]} />
+
+The high route took me to the summits of Pic des Sept Hommes and Puig Roja by an easy trail, similar to yesterday’s ridge walk. After the second peak, the ridge became increasingly rocky and steep. 
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5989.jpeg", caption: "Great view of Pic de Canigou from the ridge"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5993.jpeg", caption: "Short traverse through a boulder section"}]} />
+
+I followed some cairns to a col where I saw a faint trail going down to some lakes.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5996.jpeg", caption: "I left the ridge here to descend"}]} />
+
+Although it was incredibly steep and loose, the descent ended before I stopped having fun. At the bottom I passed three green, unswimmable lakes where I debated whether to set up camp or keep going.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6001.jpeg", caption: "Made it to the lakes"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6005.jpeg", caption: "Gorgs de Cady, the lake water was not inviting for swimming. I only dipped my feet in to wash my legs."}]} />
+
+It was only 3pm so climbing Canigou was still possible, even though the sky was filled with dark clouds. Fortunately, I had phone signal here so a quick check of the weather only showed 20% chance of rain.
+
+<p>After some thought, I decided to attempt the summit of Canigou and sleep on the other side. I didn’t want to camp here because the water did not look tasty and the ground was rough. My legs felt strong, and I had plenty of energy today.</p>
+
+I saw only one group coming down from the summit as I ascended, since this was the less popular approach to the peak. Halfway up the switchbacks, I joined back up with the HRP and came to a fountain, Font Nostre, where I filled up my water.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6010.jpeg", caption: "Approaching the top section of Canigou"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6011.jpeg", caption: "Looking back at the valley to the bowl (left side) where I descended from the ridge"}]} />
+
+The final section of the climb involved scrambling up a steep rocky chimney to gain the summit.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6015.jpeg", caption: "The chimney scramble"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6017.jpeg", caption: "A lone spire in the chimney"}]} />
+
+I had two thoughts during the scramble: please don’t rain, and I hope nobody is coming down and knocking rocks in my direction.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6020.jpeg", caption: "Steepest oart near the top"}]} />
+
+Luckily none of these things happened on my climb. During the last 10 meters I started hearing voices above me, meaning I had made it! It was a classic scramble with a top-out right at the summit.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6022.jpeg", caption: "Pic de Canigou, 2784m. My favorite summit of the HRP!"}]} />
+
+I shared the top of Canigou at 5pm with only a few others, French hikers who had ascended from the other route. I met a crazy guy who wanted to bivouac here, who I thought was joking until he pulled out his tent and started setting up in a small flat spot.
+
+<p>A clap of thunder in the distance got me back on my feet to start going down. Although the sky looked dark, it never started to rain, just like yesterday.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6032.jpeg", caption: "Interesting cloud formation on the descent to Cortalets"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6035.jpeg", caption: "Estanyol (pond)"}]} />
+
+The descent down to Refuge de Cortalets took about an hour. When I arrived I managed to convince them to let me have dinner there, even though I was arriving a day ahead of my reservation. I set up my tent in their spacious, forested bivouac area.
+
+<p>It was my first refuge dinner in a while, and I met lots of people who where looking to climb Canigou the next day.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6039.jpeg", caption: "The Refuge des Cortalets had their own beer."}]} />
+
+It has been a big day! As I settled into my tent for the night I remembered the place I woke up this morning, and it felt very far away and long ago. My scenic deviation from the HRP was not a shortcut, but definitely worth it. I also did not expect to climb Canigou, but it ended up being the perfect capstone to a great day of wandering in the mountains.
+
+<p>Pla de Coma Ermada to Refuge des Cortalets - 28.33km and 1339m gained</p>
   </Post>
 );
 
