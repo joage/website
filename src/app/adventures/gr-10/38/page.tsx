a3a687fa88cef133aee6b755d84f09f288ee2869
George Wang <wgeorge@berkeley.edu>
2023-08-27 13:56:13 +0200

    Update page.tsx 38

--- src/app/adventures/gr-10/38/page.tsx
+++ src/app/adventures/gr-10/38/page.tsx
@@ -4,30 +4,32 @@ import Post from '@/components/Post';
 const GR = () => (
   <Post title="Mérens and Ax (HRP Day 38)" date="August 17, 2023" prev="/adventures/gr-10/37" next="/adventures/gr-10/39">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5828.jpeg", caption: "Lunch in Ax"}]} />
 
 Rest day. I woke up in the campground at L’Hospitalet-près-l’Andorre. The name of this town means “hospice near Andorra”. Even by French standards it’s a weird name.
 
-Today was a day of rail travel. I took the first train in the morning to Mérens-les-Vals. This was also a tiny town but the GR 10 passes through it. I passed many hikers on my way to the hot springs.
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5821.jpeg", caption: "L'Hospitalet campground"}]} />
 
-A short but steep hike led to the springs, which had several pools. There was only one guy soaking.  Some other people were camped out nearby, still sleeping probably. I had arrived early enough!
+Today was a day of rail travel. I took the first train in the morning to Mérens-les-Vals. This was also a tiny town but the GR 10 goes through it. I passed many hikers on my way to the hot springs.
+
+<p>A short but steep walk led to the springs, which had several pools. There was only one guy soaking.  Some other people were camped out nearby, still sleeping probably. I had arrived early enough!</p>
 
 I enjoyed the warm water for twenty minutes before it started to get too hot. With my clothes back on, I hiked back down to the village. The only store and cafe was located inside the train station. I got a coffee and bought some ramen and cereal bars.
 
-The next train to Ax-les-Thermes would arrive in two hours. Checking the schedule further, I realized I could take the opposite train back to L’Hospitalet, take a shower and do my laundry, then catch the following train back to Merens and then Ax. Since I was traveling one station each time, it only cost 1.50 euro a ride but the online checkout process felt like booking a plane ticket.
+<p>The next train to Ax-les-Thermes would arrive in two hours. Checking the schedule further, I realized I could take the opposite train back to L’Hospitalet, take a shower and do my laundry, then catch the following train back to Merens and then Ax. Since I was traveling one station each time, it only cost 1.50 euro a ride but the online checkout process felt like booking a plane ticket.</p>
 
 A couple hours later, I was showered and ready to explore Ax-les-Thermes.
 
-Ax had a very scenic river walk. I had two meals here and bought groceries for the HRP, including peanut butter which is super rare in the Pyrenees!
+<p>Ax had a very scenic river walk. I had two meals here and bought groceries for the HRP, including peanut butter which is super rare in the Pyrenees!</p>
 
-At the end of the day, I hopped on the last train back to L’Hospitalet. These tiny French villages are so well connected by the SNCF. Ax was very pretty but also very touri$ty. Mérens felt rustic and old. L’hospitalet is a border town, so the bakery was busy with hungry road trippers just entering France from Andorra and Spain.
+At the end of the day, I hopped on the last train back to L’Hospitalet. These small French villages are so well connected by the SNCF. Ax was very pretty but also very touristy. Mérens felt rustic and old. L’hospitalet is a border town, so the bakery was busy with hungry road trippers just entering France from Andorra and Spain.
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5822.jpeg", caption: "Really good bakery in L'Hospitalet"}, {path: "/images/gr-10/IMG_5824.jpeg", caption: "Bridge over the Ariège at Ax"}]} />
 
-I went back to L'Hospitalet 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5832.jpeg", caption: ""}, {path: "/images/gr-10/IMG_5833.jpeg", caption: ""}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5827.jpeg", caption: "Ax-les-Thermes"}]} />
 
-<p>Coms de Jan to L'Hospitalet-près-l'Andorre - 21.37km and 1369m gained, 1984m lost</p>
   </Post>
 );
 
