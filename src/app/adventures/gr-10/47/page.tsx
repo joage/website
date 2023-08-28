813e76d2d57adddf5decfb968ebb98cb8dd08d89
George Wang <wgeorge@berkeley.edu>
2023-08-28 09:59:19 +0200

    Update page.tsx banyuls

--- src/app/adventures/gr-10/47/page.tsx
+++ src/app/adventures/gr-10/47/page.tsx
@@ -4,10 +4,19 @@ import Post from '@/components/Post';
 const GR = () => (
   <Post title="Sunrise (HRP Day 47)" date="August 26, 2023" prev="/adventures/gr-10/46">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5761.jpeg", caption: "Where I woke up"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6203.jpeg", caption: "Today is the last day of my journey"}]} />
 
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6178.jpeg", caption: "Night lights before sunrise. The land is super flat north of the Pyrenees so we could see far"}]} />
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5804.jpeg", caption: "Interesting peaks above second lake"}, {path: "/images/gr-10/IMG_5805.jpeg", caption: "Lots of boulders to traverse"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6185.jpeg", caption: "Small sliver of sun"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6217.jpeg", caption: "Final breakfast"}, {path: "/images/gr-10/IMG_6218.jpeg", caption: "😋"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6020.jpeg", caption: "Setting off"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6024.jpeg", caption: "The peak was no more than a small bump"}]} />
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6030.jpeg", caption: "Banyuls"}]} />
 
 <p>Pic des Quatre Termes to Banyuls Plage - 14.61km and 147m gained, 1257m lost</p>
   </Post>
