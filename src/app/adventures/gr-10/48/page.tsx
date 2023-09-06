577cefb7e578b853923f38bae82fa6d5b0a1e7da
George Wang <wgeorge@berkeley.edu>
2023-09-06 13:58:54 +0200

    Update page.tsx 48 a

--- src/app/adventures/gr-10/48/page.tsx
+++ src/app/adventures/gr-10/48/page.tsx
@@ -4,19 +4,19 @@ import Post from '@/components/Post';
 const GR = () => (
   <Post title="My long walk in the Pyrenees (HRP Recap)" date="September 5, 2023" prev="/adventures/gr-10/47">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_6185.jpeg", caption: "Today is the last day of my journey"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: ""}]} />
 
 Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
 
 My journey took me to 3 countries,  Spain, France and Andorra, although it felt like 5. Basque country was unique to itself, with its own language, food and culture, same with Catalonia.
 
-Each day had ups and downs like the elevation profile I followed. But over time larger sections developed in my mind, with start and end points in villages or resupply points. The scenery in the Pyrenees was as varied as the people I met. I walked with new friends each section. Many sections I walked alone.
+Following the Paul Whiteburn pocket guide (a pdf downloaded on my phone) and Mapy.cz (a free offline map app with topo lines), I navigated the mountains in weeklong sections. At the end of each section was a town, where I could buy more groceries for the next section. The scenery in the Pyrenees was as varied as the people I met. I walked with new friends each section. Many sections I walked alone.
 
 My favorite section is hard to say. Basque Country was cute and I made friends. Lescun to Gavarnie was magnificent but popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.
 
-My french has improved greatly. Talking to strangers was often a necessity in the mountains, so I learned faster than I ever did in a classroom. People in the Pyrenees are often bilingual or trilingual. Knowing one of: Spanish, French, or Catalan allows you to communicate with everyone to some degree.
+My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak Spanish or Catalan.
 
-Through numerous mistakes, I have learned many things about thru-hiking, gear, food, and pack weight that will be handy for the next adventure.
+Through numerous mistakes, I have learned things about thru-hiking, gear, food, and pack weight that will be handy for the next adventure.
 
 times hitchhiked (to go off route) 5
 
