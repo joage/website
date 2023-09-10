f3bd897055ece5ec6fa9b8c33d098ae4503ee842
George Wang <wgeorge@berkeley.edu>
2023-09-10 09:55:02 +0200

    Update page.tsx hh

--- src/app/adventures/gr-10/48/page.tsx
+++ src/app/adventures/gr-10/48/page.tsx
@@ -9,7 +9,7 @@ const GR = () => (
 Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4615.jpeg", caption: "Day 4"}, {path: "/images/gr-10/IMG_4974.jpeg", caption: "Day 14"}]} />
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5148.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5567.jpeg", caption: "Day 31"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5148.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5761.jpeg", caption: "Day 37"}]} />
 
 My journey took me to 3 countries,  Spain, France and Andorra, although it felt like 5. Basque country was unique to itself, with its own language, food and culture, same with Catalonia.
 
@@ -17,7 +17,10 @@ My journey took me to 3 countries,  Spain, France and Andorra, although it felt 
 
 Following the Paul Whiteburn pocket guide (a pdf downloaded on my phone) and Mapy.cz (a free offline map app with topo lines), I navigated the mountains in weeklong sections. At the end of each section was a town, where I could buy more groceries for the next section. The scenery in the Pyrenees was as varied as the people I met. I walked with new friends each section. Many sections I walked alone.
 
-My favorite section is hard to say. Basque Country was wet, but I made friends. Lescun to Gavarnie was magnificent and popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.
+<p>My favorite section is hard to say. Basque Country was wet, but I made friends. Lescun to Gavarnie was magnificent and popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Day 25"}, {path: "/images/gr-10/IMG_5684.jpeg", caption: "Day 34"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5127.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5561.jpeg", caption: "Day 31"}]} />
 
 My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak spanish.
 
