380e6182e4b99fa460e6a4eff60bbca0530917f7
George Wang <wgeorge@berkeley.edu>
2023-09-10 15:09:20 +0200

    Update page.tsx fixes

--- src/app/adventures/gr-10/48/page.tsx
+++ src/app/adventures/gr-10/48/page.tsx
@@ -19,37 +19,34 @@ Following the Paul Whiteburn pocket guide (a pdf downloaded on my phone) and Map
 
 <p>My favorite section is hard to say. Basque Country was wet, but I made friends. Lescun to Gavarnie was magnificent and popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.</p>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Day 25"}, {path: "/images/gr-10/IMG_5684.jpeg", caption: "Day 34"}]} />
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5127.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5561.jpeg", caption: "Day 31"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Day 25"}, {path: "/images/gr-10/8cf1fb5c-92df-4e86-b0d3-70684506ea66.jpeg", caption: "Day 34"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5127.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5567.jpeg", caption: "Day 31"}]} />
 
 My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak spanish.
 
     <ImageGroup images={[{path: "/images/gr-10/IMG_4730.jpeg", caption: "Day 8"}, {path: "/images/gr-10/IMG_6020.jpeg", caption: "Day 42"}]} />
 
 HRP totals:
-
+<div>
 <p>Days hiked: 45 + 2 rest days</p>
 <p>Nights camped (free): 24</p>
 <p>Nights camped in a paid campground: 11</p>
 <p>Nights slept in a refuge/gite: 10</p>
 <p>Nights slept in a hotel: 2</p>
-<br />
+</div>
 
 <p>Total distance: 834km (518 mi), equivalent to crossing the Golden Gate Bridge 305 times</p>
 
 <p>Total elevation ascended: 48704m (159790 ft), equivalent to climbing the Eiffel Tower 162 times</p>
-<br />
 
-
+<div>
 <p>Times hitchhiked (to go off route): 5</p>
 <p>Amount of cheese carried and eaten: 3kg</p>
 <p>Favorite cheese: Tomme de brebis
 Favorite peak: Canigou</p>
+</div>
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Day 1 Start"}, {path: "/images/gr-10/IMG_6230.jpeg", caption: "Day 47 End!"}]} />
-
-
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Day 1 Start"}, {path: "/images/gr-10/IMG_6230.jpeg", caption: "Day 47 End!"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Day 1 Start"}, {path: "/images/gr-10/IMG_6230.jpeg", caption: "Day 47 End"}]} />
 
 To my friends and family, thank you for reading my daily journal entries despite the sloppy writing and photography. I have left the mountains for the time being, but I know I will be back soon. The adventure continues!
   </Post>
