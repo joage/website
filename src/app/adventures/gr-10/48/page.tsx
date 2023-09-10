5fc04bae5f9c660e628ac270f3449a9803d226a5
George Wang <wgeorge@berkeley.edu>
2023-09-10 08:45:05 +0200

    Update page.tsx picc

--- src/app/adventures/gr-10/48/page.tsx
+++ src/app/adventures/gr-10/48/page.tsx
@@ -8,7 +8,8 @@ const GR = () => (
 
 Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_4515.jpeg", caption: "Day 4"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4515.jpeg", caption: "Day 4"}, {path: "/images/gr-10/IMG_4974.jpeg", caption: "Day 14"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5148.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5567.jpeg", caption: "Day 31"}]} />
 
 My journey took me to 3 countries,  Spain, France and Andorra, although it felt like 5. Basque country was unique to itself, with its own language, food and culture, same with Catalonia.
 
