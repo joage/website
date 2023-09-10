d79888a6faedc11bc941ecbce711f05c48d70ce9
George Wang <wgeorge@berkeley.edu>
2023-09-10 06:40:01 +0200

    Update page.tsx text

--- src/app/adventures/gr-10/48/page.tsx
+++ src/app/adventures/gr-10/48/page.tsx
@@ -4,43 +4,38 @@ import Post from '@/components/Post';
 const GR = () => (
   <Post title="My long walk in the Pyrenees (HRP Recap)" date="September 5, 2023" prev="/adventures/gr-10/47">
 
-    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: ""}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6376.jpeg", caption: "This summer I walked from the Atlantic Ocean to the Mediterranean Sea, traversing the Pyrenees mountain range in 47 days."}]} />
 
-This summer I walked from the Atlantic Ocean to the Mediterranean Sea, traversing the Pyrenees mountain range in 47 days.
+Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
 
 My journey took me to 3 countries,  Spain, France and Andorra, although it felt like 5. Basque country was unique to itself, with its own language, food and culture, same with Catalonia.
 
 Following the Paul Whiteburn pocket guide (a pdf downloaded on my phone) and Mapy.cz (a free offline map app with topo lines), I navigated the mountains in weeklong sections. At the end of each section was a town, where I could buy more groceries for the next section. The scenery in the Pyrenees was as varied as the people I met. I walked with new friends each section. Many sections I walked alone.
 
-My favorite section is hard to say. Basque Country was cute and I made friends. Lescun to Gavarnie was magnificent but popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.
+My favorite section is hard to say. Basque Country was wet, but I made friends. Lescun to Gavarnie was magnificent and popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.
 
-My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak Spanish or Catalan.
+My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak spanish.
 
-Through numerous mistakes, I have learned things about thru-hiking, gear, food, and pack weight that will be handy for the next adventure.
+To my friends and family, thank you for reading my daily journal entries. I have left the mountains for the time being, but looking back on this blog will remind me of the fond memories I made this summer. The adventure continues!
 
-Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
+Days hiked: 45 + 2 rest days
+Nights camped (free): 24
+Nights camped in a paid campground: 11
+Nights slept in a refuge/gite: 10
+Nights slept in a hotel: 2
 
-times hitchhiked (to go off route) 5
 
-kgs of cheese carried and eaten: at least 4
+Total distance: 834km (518 mi), equivalent to crossing the Golden Gate Bridge 305 times
+
+Total elevation ascended: 48704m (159790 ft), equivalent to climbing the Eiffel Tower 162 times
+
+Times hitchhiked (to go off route): 5
+
+Amount of cheese carried and eaten: 3kg
 Favorite cheese: Tomme de brebis
 
 Favorite peak: Canigou
 
-days hiked: 45 + 2 rest days
-nights camped (free): 24
-nights camped in a paid campground: 11
-nights slept in a refuge/gite: 10
-nights slept in a hotel: 2
-
-
-kms hiked: 834 km
-miles hiked: 518 mi
-crossing the golden gate bridge 305 times
-
-m ascended: 48704 m
-ft ascended 159790 ft
-climbing the eiffel tower 162 times
   </Post>
 );
 
