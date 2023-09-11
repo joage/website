275d2bd413b17bc55d1c9d6d8168d3a7b906db24
George Wang <georgeyw@gmail.com>
2023-09-11 08:46:49 +0200

    Toulouse (#18)

---
+++ src/app/adventures/gr-10/48/page.tsx
@@ -0,0 +1,60 @@
+import ImageGroup from '@/components/ImageGroup';
+import Post from '@/components/Post';
+
+const GR = () => (
+  <Post title="My long walk in the Pyrenees (HRP Recap)" date="September 5, 2023" prev="/adventures/gr-10/47">
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4615.jpeg", caption: "Day 4"}, {path: "/images/gr-10/IMG_4974.jpeg", caption: "Day 14"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5148.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5761.jpeg", caption: "Day 37"}]} />
+
+Getting off the train in Hendaye intending to do the GR 10, I remember an interaction I had with a German guy who was starting the Haute Route Pyrénéene (HRP). As he sped off, leaving me in his dust, I thought to myself that the HRP would be way too difficult and out of my reach. A week later, I met Tristan and Tania, who took me off the GR to walk the ridge of Pic d'Orhi on a section of the HRP. From that point onwards I was hooked on the beauty of the Haute Route. I never came back to the GR 10, except where the two trails collided. Reaching the Mediterranean 47 days later and thinking back to this interaction, I feel a sense of accomplishment that was hard to imagine after each long day of trekking, each grueling climb.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_6376.jpeg", caption: "This summer I walked from the Atlantic Ocean to the Mediterranean Sea, traversing the Pyrenees mountain range in 47 days."}]} />
+
+My journey took me to 3 countries,  Spain, France and Andorra, although it felt like 5. Basque country was unique to itself, with its own language, food and culture, same with Catalonia.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5318.jpeg", caption: "Day 22"}, {path: "/images/gr-10/IMG_5425.jpeg", caption: "Day 26"}]} />
+
+Following the Paul Whiteburn pocket guide (a pdf downloaded on my phone) and Mapy.cz (a free offline map app with topo lines), I navigated the mountains in weeklong sections. At the end of each section was a town, where I could buy more groceries for the next section. The scenery in the Pyrenees was as varied as the people I met. I walked with new friends each section. Many sections I walked alone.
+
+<p>My favorite section is hard to say. Basque Country was wet, but I made friends. Lescun to Gavarnie was magnificent and popular. Gavarnie to Salardu was the most epic and difficult. Salardu to L’Hospitalet was wild and lonely. L’Hospitalet to Banyuls was surprisingly beautiful, and hot towards the end.</p>
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5396.jpeg", caption: "Day 25"}, {path: "/images/gr-10/8cf1fb5c-92df-4e86-b0d3-70684506ea66.jpeg", caption: "Day 34"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5127.jpeg", caption: "Day 18"}, {path: "/images/gr-10/IMG_5567.jpeg", caption: "Day 31"}]} />
+
+My french has gotten better. Talking to strangers was often a necessity in the mountains, so I improved faster than I ever did in a classroom. However I still cannot speak spanish.
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4730.jpeg", caption: "Day 8"}, {path: "/images/gr-10/IMG_6020.jpeg", caption: "Day 42"}]} />
+
+Walking alone, I met so many amazing people who kept me sane and safe on the trail. Thank you for sharing a part of this adventure with me!
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_0284.jpeg", caption: "Tania, Tristan"}, {path: "/images/gr-10/CD434F41-8F0F-40EB-AF7A-5E9C37DF5335.jpeg", caption: "Leo, Camille"}]} />
+    <ImageGroup images={[{path: "/images/IMG_5024.jpeg", caption: "Jacques"}, {path: "/images/gr-10/IMG_5424.jpeg", caption: "Thivo, Jade"}]} />
+    <ImageGroup images={[{path: "/images/gr-10/IMG_5462.jpeg", caption: "Benjamin, Dan"}, {path: "/images/gr-10/IMG_5687.jpeg", caption: "Pavol"}]} />
+
+The HRP was my first long hike, and my first time camping alone. After more than 2 months in France and Spain, I am done hiking for the time being. I have left the mountains to return to city life, but I know I will be back soon. The adventure continues! 💠
+
+    <ImageGroup images={[{path: "/images/gr-10/IMG_4472.jpeg", caption: "Day 1 Start"}, {path: "/images/gr-10/IMG_6230.jpeg", caption: "Day 47 End"}]} />
+
+HRP totals:
+<div>
+<p>Days hiked: 45 + 2 rest days</p>
+<p>Nights camped (free): 24</p>
+<p>Nights camped in a paid campground: 11</p>
+<p>Nights slept in a refuge/gite: 10</p>
+<p>Nights slept in a hotel: 2</p>
+</div>
+
+<p>Total distance: 834km (518 mi), equivalent to crossing the Golden Gate Bridge 305 times</p>
+
+<p>Total elevation ascended: 48704m (159790 ft), equivalent to climbing the Eiffel Tower 162 times</p>
+
+<div>
+<p>Times hitchhiked (to go off route): 5</p>
+<p>Amount of cheese carried and eaten: 3kg</p>
+<p>Favorite cheese: Tomme de brebis</p>
+<p>Favorite peak: Canigou</p>
+</div>
+  </Post>
+);
+
+export default GR;
