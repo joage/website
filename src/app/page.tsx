24621a4d3e71ef95d4dc212005ebc7e5ce8db6e4
George Wang <georgeyw@gmail.com>
2023-10-04 18:00:17 -0400

    Tweak homepage (#21)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import ImageGroup from "@/components/ImageGroup";
+import Image from "next/image";
 import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center p-4">
       <Nav />
-      <p className="text-sm pt-8">
+      <p className="text-sm pt-8 max-w-xl text-center">
         Hi my name is George. I write code for work, and I like walking in the
         mountains. Welcome to my website!
       </p>
-      <div className="pt-3">
-        <ImageGroup images={[{ path: "/images/falling.jpeg", caption: "" }]} />
+      <div className="pt-7">
+        <Image
+          src="/images/falling.jpeg"
+          alt="/images/falling.jpeg"
+          width={400}
+          height={500}
+        />
       </div>
     </main>
   );
