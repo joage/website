62510f4d2f5265912cf8e457929222f394509477
George Wang <georgeyw@gmail.com>
2024-11-08 18:06:07 -0800

    fix issue

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -73,7 +73,6 @@ export default function Adventures() {
                   SHR Day 10 - Shout of Relief
                 </Link>
               </li>
-<<<<<<< HEAD
               <li>
                 <Link href="/adventures/shr/11">SHR Day 11 - Beach Day</Link>
               </li>
@@ -83,8 +82,6 @@ export default function Adventures() {
               <li>
                 <Link href="/adventures/shr/13">SHR Day 13 - Bishop</Link>
               </li>
-=======
->>>>>>> 3c7cba60f1183d42308911e11d0e99830d3f7368
             </details>
           </ul>
         </div>
